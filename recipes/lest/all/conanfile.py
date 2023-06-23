81df22c08c6e934397fbe5ec80c6596fbc8cb36c
Martin Valgur <martin.valgur@gmail.com>
2023-06-23 03:27:48 +0300

    lest: migrate to Conan v2

--- recipes/lest/all/conanfile.py
+++ recipes/lest/all/conanfile.py
@@ -1,102 +1,40 @@
-# TODO: verify the Conan v2 migration
-
 import os
 
-from conan import ConanFile, conan_version
-from conan.errors import ConanInvalidConfiguration, ConanException
-from conan.tools.android import android_abi
-from conan.tools.apple import (
-    XCRun,
-    fix_apple_shared_install_name,
-    is_apple_os,
-    to_apple_arch,
-)
-from conan.tools.build import (
-    build_jobs,
-    can_run,
-    check_min_cppstd,
-    cross_building,
-    default_cppstd,
-    stdcpp_library,
-    valid_min_cppstd,
-)
-from conan.tools.cmake import (
-    CMake,
-    CMakeDeps,
-    CMakeToolchain,
-    cmake_layout,
-)
-from conan.tools.env import (
-    Environment,
-    VirtualBuildEnv,
-    VirtualRunEnv,
-)
-from conan.tools.files import (
-    apply_conandata_patches,
-    chdir,
-    collect_libs,
-    copy,
-    download,
-    export_conandata_patches,
-    get,
-    load,
-    mkdir,
-    patch,
-    patches,
-    rename,
-    replace_in_file,
-    rm,
-    rmdir,
-    save,
-    symlinks,
-    unzip,
-)
-from conan.tools.gnu import (
-    Autotools,
-    AutotoolsDeps,
-    AutotoolsToolchain,
-    PkgConfig,
-    PkgConfigDeps,
-)
+from conan import ConanFile
+from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
-from conan.tools.meson import MesonToolchain, Meson
-from conan.tools.microsoft import (
-    MSBuild,
-    MSBuildDeps,
-    MSBuildToolchain,
-    NMakeDeps,
-    NMakeToolchain,
-    VCVars,
-    check_min_vs,
-    is_msvc,
-    is_msvc_static_runtime,
-    msvc_runtime_flag,
-    unix_path,
-    unix_path_package_info_legacy,
-    vs_layout,
-)
-from conan.tools.microsoft.visual import vs_ide_version
-from conan.tools.scm import Version
-from conan.tools.system import package_manager
 
 
 class LestConan(ConanFile):
     name = "lest"
     description = (
-        "A modern, C++11-native, single-file header-only, tiny framework for unit-tests, TDD and BDD."
+        "A modern, C++11-native, single-file header-only, "
+        "tiny framework for unit-tests, TDD and BDD."
     )
     license = "BSL-1.0"
-    topics = ("testing", "testing-framework", "unit-testing", "header-only")
-    homepage = "https://github.com/martinmoene/lest"
     url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/martinmoene/lest"
+    topics = ("testing", "testing-framework", "unit-testing", "header-only")
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        os.rename(self.name + "-" + self.version, self.source_folder)
 
     def package(self):
-        copy(self, "LICENSE.txt", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(
+            self,
+            "LICENSE.txt",
+            dst=os.path.join(self.package_folder, "licenses"),
+            src=self.source_folder,
+        )
         copy(
             self,
             pattern="*.hpp",
@@ -104,5 +42,6 @@ class LestConan(ConanFile):
             src=os.path.join(self.source_folder, "include"),
         )
 
-    def package_id(self):
-        self.info.header_only()
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
