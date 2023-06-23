2f70e93471a32360d35e5b08545fa4f1e66513bf
Martin Valgur <martin.valgur@gmail.com>
2023-06-23 16:46:27 +0300

    darknet: migrate to Conan v2

--- recipes/darknet/all/conanfile.py
+++ recipes/darknet/all/conanfile.py
@@ -1,83 +1,16 @@
-# TODO: verify the Conan v2 migration
-
 import os
 
-from conan import ConanFile, conan_version
-from conan.errors import ConanInvalidConfiguration, ConanException
-from conan.tools.android import android_abi
-from conan.tools.apple import (
-    XCRun,
-    fix_apple_shared_install_name,
-    is_apple_os,
-    to_apple_arch,
-)
-from conan.tools.build import (
-    build_jobs,
-    can_run,
-    check_min_cppstd,
-    cross_building,
-    default_cppstd,
-    stdcpp_library,
-    valid_min_cppstd,
-)
-from conan.tools.cmake import (
-    CMake,
-    CMakeDeps,
-    CMakeToolchain,
-    cmake_layout,
-)
-from conan.tools.env import (
-    Environment,
-    VirtualBuildEnv,
-    VirtualRunEnv,
-)
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import stdcpp_library
 from conan.tools.files import (
     apply_conandata_patches,
-    chdir,
-    collect_libs,
     copy,
-    download,
     export_conandata_patches,
     get,
-    load,
-    mkdir,
-    patch,
-    patches,
-    rename,
     replace_in_file,
-    rm,
-    rmdir,
-    save,
-    symlinks,
-    unzip,
 )
-from conan.tools.gnu import (
-    Autotools,
-    AutotoolsDeps,
-    AutotoolsToolchain,
-    PkgConfig,
-    PkgConfigDeps,
-)
-from conan.tools.layout import basic_layout
-from conan.tools.meson import MesonToolchain, Meson
-from conan.tools.microsoft import (
-    MSBuild,
-    MSBuildDeps,
-    MSBuildToolchain,
-    NMakeDeps,
-    NMakeToolchain,
-    VCVars,
-    check_min_vs,
-    is_msvc,
-    is_msvc_static_runtime,
-    msvc_runtime_flag,
-    unix_path,
-    unix_path_package_info_legacy,
-    vs_layout,
-)
-from conan.tools.microsoft.visual import vs_ide_version
-from conan.tools.scm import Version
-from conan.tools.system import package_manager
+from conan.tools.gnu import Autotools, AutotoolsToolchain, PkgConfigDeps
 
 
 class DarknetConan(ConanFile):
@@ -98,7 +31,6 @@ class DarknetConan(ConanFile):
         "fPIC": True,
         "with_opencv": False,
     }
-    generators = "pkg_config"
 
     @property
     def _lib_to_compile(self):
@@ -120,7 +52,7 @@ class DarknetConan(ConanFile):
             self,
             os.path.join(self.source_folder, "Makefile"),
             "SLIB=libdarknet.so",
-            "SLIB=libdarknet" + self._shared_lib_extension,
+            f"SLIB=libdarknet{self._shared_lib_extension}",
         )
         replace_in_file(
             self,
@@ -149,30 +81,43 @@ class DarknetConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
+    def generate(self):
+        tc = AutotoolsToolchain(self)
+        tc.make_args = ["OPENCV={}".format("1" if self.options.with_opencv else "0")]
+        tc.generate()
+        tc = PkgConfigDeps(self)
+        tc.generate()
+
     def build(self):
         self._patch_sources()
-        with chdir(self, self.source_folder):
-            with environment_append(self, {"PKG_CONFIG_PATH": self.build_folder}):
-                args = ["OPENCV={}".format("1" if self.options.with_opencv else "0")]
-                env_build = AutoToolsBuildEnvironment(self)
-                env_build.fpic = self.options.get_safe("fPIC", True)
-                env_build.make(args=args)
+        autotools = Autotools(self)
+        autotools.make()
 
     def package(self):
-        copy(self, "LICENSE*", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(
+            self,
+            "LICENSE*",
+            dst=os.path.join(self.package_folder, "licenses"),
+            src=self.source_folder,
+        )
         copy(
             self,
             "*.h",
             dst=os.path.join(self.package_folder, "include"),
             src=os.path.join(self.source_folder, "include"),
         )
-        copy(self, "*.so", dst=os.path.join(self.package_folder, "lib"), keep_path=False)
-        copy(self, "*.dylib", dst=os.path.join(self.package_folder, "lib"), keep_path=False)
-        copy(self, "*.a", dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        for pattern in ["*.so", "*.dylib", "*.a"]:
+            copy(
+                self,
+                pattern,
+                src=self.build_folder,
+                dst=os.path.join(self.package_folder, "lib"),
+                keep_path=False,
+            )
 
     def package_info(self):
         self.cpp_info.libs = ["darknet"]
-        if self.settings.os == "Linux":
+        if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs = ["m", "pthread"]
         if stdcpp_library(self):
             self.cpp_info.system_libs.append(stdcpp_library(self))
