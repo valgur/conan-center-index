e06e87f4e0a91b99029aa6a2cb25f1e85ccd51e9
Martin Valgur <martin.valgur@gmail.com>
2023-06-30 14:04:42 +0300

    btyacc: migrate to Conan v2, WIP

--- recipes/btyacc/all/conanfile.py
+++ recipes/btyacc/all/conanfile.py
@@ -1,19 +1,22 @@
-import functools
 import os
 import textwrap
 
-from conans import CMake, ConanFile, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get, rmdir, save
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.52.0"
 
 
 class BtyaccConan(ConanFile):
     name = "btyacc"
+    description = "Backtracking yacc"
+    license = "Unlicense"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/ChrisDodd/btyacc"
-    description = "Backtracking yacc"
-    topics = "yacc", "parser"
-    license = "Unlicense"
+    topics = ("yacc", "parser", "header-only")
+
+    package_type = "application"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "fPIC": [True, False],
@@ -21,45 +24,48 @@ class BtyaccConan(ConanFile):
     default_options = {
         "fPIC": True,
     }
-    generators = "cmake"
-    exports_sources = "CMakeLists.txt"
     no_copy_source = True
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
 
     def source(self):
-        root = self._source_subfolder
+        root = self.source_folder
         get_args = self.conan_data["sources"][self.version]
-        tools.get(**get_args, destination=root, strip_root=True)
+        get(self, **get_args, destination=root, strip_root=True)
 
-    @functools.lru_cache(1)
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure()
-        return cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        tc = CMakeDeps(self)
+        tc.generate()
 
     def build(self):
-        self._configure_cmake().build()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     @property
     def _variables(self):
         return os.path.join("bin", "conan-official-btyacc-variables.cmake")
 
     def package(self):
-        self.copy("README", "licenses", self._source_subfolder)
-        self.copy("README.BYACC", "licenses", self._source_subfolder)
-        self._configure_cmake().install()
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        copy(self, "README", "licenses", self.source_folder)
+        copy(self, "README.BYACC", "licenses", self.source_folder)
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "share"))
         variables = os.path.join(self.package_folder, self._variables)
         content = textwrap.dedent("""\
             set(BTYACC_EXECUTABLE "${CMAKE_CURRENT_LIST_DIR}/btyacc")
@@ -67,14 +73,19 @@ class BtyaccConan(ConanFile):
               set(BTYACC_EXECUTABLE "${BTYACC_EXECUTABLE}.exe")
             endif()
         """)
-        tools.save(variables, content)
+        save(self, variables, content)
 
     def package_info(self):
+        self.cpp_info.includedirs = []
+        self.cpp_info.frameworkdirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
+
         bindir = os.path.join(self.package_folder, "bin")
+        self.cpp_info.bindirs = [bindir]
         self.output.info(f"Appending PATH environment variable: {bindir}")
         self.env_info.PATH.append(bindir)
         self.cpp_info.build_modules["cmake"] = [self._variables]
         self.cpp_info.build_modules["cmake_find_package"] = [self._variables]
-        self.cpp_info.build_modules["cmake_find_package_multi"] = \
-            [self._variables]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._variables]
         self.cpp_info.builddirs = ["bin"]
