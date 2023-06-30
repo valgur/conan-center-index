83caab28bd57bab324bc50efc8e5b094e6b18c7b
Martin Valgur <martin.valgur@gmail.com>
2023-06-30 08:55:38 +0300

    platform.hashing: migrate to Conan v2

--- recipes/platform.hashing/all/test_package/conanfile.py
+++ recipes/platform.hashing/all/test_package/conanfile.py
@@ -1,28 +1,46 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanException
 import os
+
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
+from conan.tools.microsoft import is_msvc
+from conans.errors import ConanInvalidConfiguration
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
 
     @property
     def _extra_flags(self):
-        return self.deps_user_info["platform.hashing"].suggested_flags
+        return self.conf_info.get("user.platform.hashing:suggested_flags")
+
+    def validate(self):
+        if not is_msvc(self):
+            if not self._extra_flags:
+                raise ConanInvalidConfiguration(
+                    f"Suggested flags are not available for os={self.settings.os}/arch={self.settings.arch}"
+                )
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        if not is_msvc(self):
+            tc.variables["EXTRA_FLAGS"] = self._extra_flags
+        tc.generate()
 
     def build(self):
-        if self.settings.compiler != "Visual Studio":
-            if not self._extra_flags:
-                raise ConanException("Suggested flags are not available for os={}/arch={}".format(self.settings.os, self.settings.arch))
-
         cmake = CMake(self)
-        if self.settings.compiler != "Visual Studio":
-            cmake.definitions["EXTRA_FLAGS"] = self._extra_flags
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
