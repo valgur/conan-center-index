0a187fa0963632b00d706d71c156cfb311c5b896
Martin Valgur <martin.valgur@gmail.com>
2023-07-27 21:40:42 +0300

    btyacc: fix test_package on Windows

--- recipes/btyacc/all/test_package/conanfile.py
+++ recipes/btyacc/all/test_package/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake
-import os
 
 
 class TestPackageConan(ConanFile):
@@ -11,6 +10,9 @@ class TestPackageConan(ConanFile):
 
     def requirements(self):
         self.requires(self.tested_reference_str)
+
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
 
     def layout(self):
         cmake_layout(self)
@@ -22,5 +24,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
-            self.run(bin_path, env="conanrun")
+            cmake = CMake(self)
+            cmake.test()
