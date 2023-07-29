6048281b1a9fcb43b7bb7f6018348feae8657d35
Martin Valgur <martin.valgur@gmail.com>
2023-07-29 15:40:11 +0300

    enchant: convert to autotools-based build

--- recipes/enchant/all/conanfile.py
+++ recipes/enchant/all/conanfile.py
@@ -1,16 +1,20 @@
+import os
+
 from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.build import cross_building
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv, Environment
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, chdir, replace_in_file
+from conan.tools.gnu import AutotoolsToolchain, AutotoolsDeps, Autotools
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, unix_path
 
 required_conan_version = ">=1.53.0"
 
 
 class EnchantConan(ConanFile):
     name = "enchant"
-    description = (
-        "Enchant aims to provide a simple but comprehensive abstraction for "
-        "dealing with different spell checking libraries in a consistent way"
-    )
+    description = ("Enchant aims to provide a simple but comprehensive abstraction for "
+                   "dealing with different spell-checking libraries in a consistent way")
     license = "LGPL-2.1-or-later"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://abiword.github.io/enchant/"
@@ -27,10 +31,11 @@ class EnchantConan(ConanFile):
         "fPIC": True,
     }
 
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
     def export_sources(self):
-        copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder)
-        copy(self, "configmake.h", src=self.recipe_folder, dst=self.export_sources_folder)
-        copy(self, "configure.cmake", src=self.recipe_folder, dst=self.export_sources_folder)
         export_conandata_patches(self)
 
     def config_options(self):
@@ -42,32 +47,65 @@ class EnchantConan(ConanFile):
             self.options.rm_safe("fPIC")
 
     def layout(self):
-        cmake_layout(self, src_folder="src")
+        basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("glib/2.76.3")
+        self.requires("glib/2.77.0")
         self.requires("hunspell/1.7.0")
+
+    def build_requirements(self):
+        if self._settings_build.os == "Windows":
+            self.win_bash = True
+            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
+                self.tool_requires("msys2/cci.latest")
+            self.tool_requires("winflexbison/2.5.24")
+        if is_msvc(self):
+            self.tool_requires("automake/1.16.5")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
-        tc = CMakeToolchain(self)
-        tc.variables["CONAN_enchant_VERSION"] = self.version
+        env = VirtualBuildEnv(self)
+        env.generate()
+        if not cross_building(self):
+            env = VirtualRunEnv(self)
+            env.generate()
+
+        tc = AutotoolsToolchain(self)
         tc.generate()
-        tc = CMakeDeps(self)
+        tc = AutotoolsDeps(self)
         tc.generate()
+
+        if is_msvc(self):
+            env = Environment()
+            automake_conf = self.dependencies.build["automake"].conf_info
+            compile_wrapper = unix_path(self, automake_conf.get("user.automake:compile-wrapper", check_type=str))
+            env.define("CC", f"{compile_wrapper} cl -nologo")
+            env.define("CXX", f"{compile_wrapper} cl -nologo")
+            env.define("LD", "link -nologo")
+            # ar-lib wrapper is added automatically by ./configure
+            # env.define("AR", f'{ar_wrapper} "lib -nologo"')
+            env.define("NM", "dumpbin -symbols")
+            env.define("OBJDUMP", ":")
+            env.define("RANLIB", ":")
+            env.define("STRIP", ":")
+            env.vars(self).save_script("conanbuild_msvc")
 
     def build(self):
         apply_conandata_patches(self)
-        cmake = CMake(self)
-        cmake.configure(build_script_folder=self.export_sources_folder)
-        cmake.build()
+        with chdir(self, self.source_folder):
+            autotools = Autotools(self)
+            os.environ["M4"] = ""
+            self.run("autoreconf -fiv")
+            autotools.configure()
+            autotools.make()
 
     def package(self):
-        copy(self, "COPYING.LIB", "licenses", self.source_folder)
-        cmake = CMake(self)
-        cmake.install()
+        copy(self, "COPYING.LIB", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        with chdir(self, self.source_folder):
+            autotools = Autotools(self)
+            autotools.install()
 
     def package_info(self):
         self.cpp_info.libs = ["enchant"]
