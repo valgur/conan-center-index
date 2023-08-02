b03ae4f96a1caa34d6cb453ca52bbb758590bc33
Martin Valgur <martin.valgur@gmail.com>
2023-08-02 20:27:01 +0300

    gpertools: add MSBuild support for Windows, WIP

--- recipes/gperftools/all/conanfile.py
+++ recipes/gperftools/all/conanfile.py
@@ -8,6 +8,7 @@ from conan.tools.cmake import cmake_layout
 from conan.tools.env import VirtualRunEnv
 from conan.tools.files import get, copy, rm, rmdir, replace_in_file
 from conan.tools.gnu import AutotoolsToolchain, AutotoolsDeps, Autotools
+from conan.tools.microsoft import is_msvc, MSBuildToolchain, MSBuildDeps, MSBuild
 
 required_conan_version = ">=1.53.0"
 
@@ -19,6 +20,7 @@ class GperftoolsConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/gperftools/gperftools"
     topics = ("memory", "allocator", "tcmalloc", "google-perftools")
+
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -87,102 +89,144 @@ class GperftoolsConan(ConanFile):
     def layout(self):
         cmake_layout(self, src_folder="src")
 
-    def validate(self):
-        if self.settings.os == "Windows":
-            raise ConanInvalidConfiguration(
-                "gperftools recipe does not currently support Windows. Contributions are welcome."
-            )
-
     def requirements(self):
-        if self.options.get_safe("enable_libunwind", False):
-            self.requires("libunwind/1.6.2")
+        if self.options.get_safe("enable_libunwind"):
+            self.requires("libunwind/1.7.0")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    @property
+    def _msbuild_configuration(self):
+        # TODO: add option to select override vs patch to
+        # "allow users to choose to override malloc/free/etc on Windows rather than patching.
+        # This can be used to create custom CRTs."
+        return  "Debug" if self.settings.build_type == "Debug" else "Release-Patch"
 
     def generate(self):
         if not cross_building(self):
             env = VirtualRunEnv(self)
             env.generate(scope="build")
 
-        tc = AutotoolsToolchain(self)
-        args = {}
-        args["prefix"] = ""
-        args["enable-cpu-profiler"] = self.options.build_cpu_profiler
-        args["enable-heap-profiler"] = self.options.build_heap_profiler
-        args["enable-heap-checker"] = self.options.build_heap_checker
-        args["enable-debugalloc"] = self.options.build_debugalloc
-        args["enable-minimal"] = self._build_minimal
-        args["enable-dynamic-sized-delete-support"] = self.options.dynamic_sized_delete_support
-        args["enable-sized-delete"] = self.options.sized_delete
-        args["enable-large-alloc-report"] = self.options.enable_large_alloc_report
-        args["enable-aggressive-decommit-by-default"] = self.options.enable_aggressive_decommit_by_default
-        if self._build_minimal:
-            # No stack trace support will be built
-            args["enable-libunwind"] = False
-            args["enable-frame-pointers"] = False
-            args["enable-stacktrace-via-backtrace"] = False
-            args["enable-emergency-malloc"] = False
+        if is_msvc(self):
+            tc = MSBuildToolchain(self)
+            tc.configuration = self._msbuild_configuration
+            # TODO: apply options
+            # TODO: disable building of executables and unit tests
+            tc.generate()
+
+            deps = MSBuildDeps(self)
+            tc.configuration = self._msbuild_configuration
+            deps.generate()
+
+            platform_toolset = MSBuildToolchain(self).toolset
+            import_conan_generators = ""
+            for props_file in ["conantoolchain.props", "conandeps.props"]:
+                props_path = os.path.join(self.generators_folder, props_file)
+                if os.path.exists(props_path):
+                    import_conan_generators += f'<Import Project="{props_path}" />'
+            for vcxproj_file in (self.source_path / "vsprojects").rglob("*.vcxproj"):
+                replace_in_file(self, vcxproj_file,
+                                "<PlatformToolset>v140</PlatformToolset>",
+                                f"<PlatformToolset>{platform_toolset}</PlatformToolset>")
+                if props_path:
+                    replace_in_file(self, vcxproj_file,
+                                    r'<Import Project="$(VCTargetsPath)\Microsoft.Cpp.targets" />',
+                                    rf'{import_conan_generators}<Import Project="$(VCTargetsPath)\Microsoft.Cpp.targets" />')
+
         else:
-            args["enable-libunwind"] = self.options.enable_libunwind
-            args["enable-frame-pointers"] = self.options.enable_frame_pointers
-            args["enable-stacktrace-via-backtrace"] = self.options.get_safe(
-                "enable_stacktrace_via_backtrace", False
-            )
-            args["enable-emergency-malloc"] = self.options.emergency_malloc
-        args["with-tcmalloc-alignment"] = self.options.tcmalloc_alignment
-        args["with-tcmalloc-pagesize"] = self.options.tcmalloc_pagesize
+            tc = AutotoolsToolchain(self)
+            args = {}
+            args["prefix"] = ""
+            args["enable-cpu-profiler"] = self.options.build_cpu_profiler
+            args["enable-heap-profiler"] = self.options.build_heap_profiler
+            args["enable-heap-checker"] = self.options.build_heap_checker
+            args["enable-debugalloc"] = self.options.build_debugalloc
+            args["enable-minimal"] = self._build_minimal
+            args["enable-dynamic-sized-delete-support"] = self.options.dynamic_sized_delete_support
+            args["enable-sized-delete"] = self.options.sized_delete
+            args["enable-large-alloc-report"] = self.options.enable_large_alloc_report
+            args["enable-aggressive-decommit-by-default"] = self.options.enable_aggressive_decommit_by_default
+            if self._build_minimal:
+                # No stack trace support will be built
+                args["enable-libunwind"] = False
+                args["enable-frame-pointers"] = False
+                args["enable-stacktrace-via-backtrace"] = False
+                args["enable-emergency-malloc"] = False
+            else:
+                args["enable-libunwind"] = self.options.enable_libunwind
+                args["enable-frame-pointers"] = self.options.enable_frame_pointers
+                args["enable-stacktrace-via-backtrace"] = self.options.get_safe("enable_stacktrace_via_backtrace", False)
+                args["enable-emergency-malloc"] = self.options.emergency_malloc
+            args["with-tcmalloc-alignment"] = self.options.tcmalloc_alignment
+            args["with-tcmalloc-pagesize"] = self.options.tcmalloc_pagesize
 
-        # Based on https://github.com/conan-io/conan-center-index/blob/c647b1/recipes/libx264/all/conanfile.py#L94
-        if is_apple_os(self) and self.settings.arch == "armv8":
-            args["host"] = "aarch64-apple-darwin"
-            tc.extra_asflags = ["-arch arm64"]
-            tc.extra_ldflags = ["-arch arm64"]
-            if self.settings.os != "Macos":
-                xcrun = XCRun(self)
-                platform_flags = ["-isysroot", xcrun.sdk_path]
-                apple_min_version_flag = AutotoolsToolchain(self).apple_min_version_flag
-                if apple_min_version_flag:
-                    platform_flags.append(apple_min_version_flag)
-                tc.extra_asflags.extend(platform_flags)
-                tc.extra_cflags.extend(platform_flags)
-                tc.extra_ldflags.extend(platform_flags)
+            # Based on https://github.com/conan-io/conan-center-index/blob/c647b1/recipes/libx264/all/conanfile.py#L94
+            if is_apple_os(self) and self.settings.arch == "armv8":
+                args["host"] = "aarch64-apple-darwin"
+                tc.extra_asflags = ["-arch arm64"]
+                tc.extra_ldflags = ["-arch arm64"]
+                if self.settings.os != "Macos":
+                    xcrun = XCRun(self)
+                    platform_flags = ["-isysroot", xcrun.sdk_path]
+                    apple_min_version_flag = AutotoolsToolchain(self).apple_min_version_flag
+                    if apple_min_version_flag:
+                        platform_flags.append(apple_min_version_flag)
+                    tc.extra_asflags.extend(platform_flags)
+                    tc.extra_cflags.extend(platform_flags)
+                    tc.extra_ldflags.extend(platform_flags)
 
-        for k, v in args.items():
-            if v in [True, False]:
-                v = "yes" if v else "no"
-            if v is not None:
-                tc.configure_args.append(f"--{k}={v}")
-        tc.generate()
+            for k, v in args.items():
+                if v in [True, False]:
+                    v = "yes" if v else "no"
+                if v is not None:
+                    tc.configure_args.append(f"--{k}={v}")
+            tc.generate()
 
-        tc = AutotoolsDeps(self)
-        tc.generate()
+            deps = AutotoolsDeps(self)
+            deps.generate()
 
     def _patch_sources(self):
         # Disable building of tests and benchmarks in Makefile
         for pattern in ["noinst_PROGRAMS = ", "TESTS = "]:
-            replace_in_file(
-                self,
-                os.path.join(self.source_folder, "Makefile.in"),
-                pattern,
-                f"{pattern}\n_{pattern}",
-            )
+            replace_in_file(self, os.path.join(self.source_folder, "Makefile.in"),
+                            pattern, f"{pattern}\n_{pattern}")
 
     def build(self):
-        self._patch_sources()
-        autotools = Autotools(self)
-        autotools.configure()
-        autotools.make()
+        if is_msvc(self):
+            msbuild = MSBuild(self)
+            msbuild.build_type = self._msbuild_configuration
+            msbuild.build(os.path.join(self.source_folder, "gperftools.sln"))
+        else:
+            self._patch_sources()
+            autotools = Autotools(self)
+            autotools.configure()
+            autotools.make()
 
     def package(self):
-        copy(
-            self,
-            pattern="COPYING",
-            dst=os.path.join(self.package_folder, "licenses"),
-            src=self.source_folder,
-        )
-        autotools = Autotools(self)
-        autotools.install()
+        copy(self, "COPYING",
+             dst=os.path.join(self.package_folder, "licenses"),
+             src=self.source_folder)
+
+        if is_msvc(self):
+            copy(self, "*.h",
+                 dst=os.path.join(self.package_folder, "include", "google"),
+                 src=os.path.join(self.source_folder, "src", "google"),
+                 keep_path=False)
+            # FIXME: tcmalloc.h needs to be generated from cmake/tcmalloc.h.in
+            copy(self, "tcmalloc.h",
+                 dst=os.path.join(self.package_folder, "include", "gperftools"),
+                 src=os.path.join(self.source_folder, "src", "gperftools"),
+                 keep_path=False)
+            for path in self.source_path.rglob("*.lib"):
+                if "test" not in path.name:
+                    copy(self, path.name, dst=self.package_path / "lib", src=path.parent)
+            for path in self.source_path.rglob("*.dll"):
+                if "test" not in path.name:
+                    copy(self, path.name, dst=self.package_path / "bin", src=path.parent)
+        else:
+            autotools = Autotools(self)
+            autotools.install()
 
         rmdir(self, os.path.join(self.package_folder, "share"))
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
@@ -190,7 +234,8 @@ class GperftoolsConan(ConanFile):
         fix_apple_shared_install_name(self)
 
     def _add_component(self, lib):
-        self.cpp_info.components[lib].libs = [lib]
+        prefix = "lib" if self.settings.os == "Windows" else ""
+        self.cpp_info.components[lib].libs = [f"{prefix}{lib}"]
 
     def package_info(self):
         self._add_component("tcmalloc_minimal")
@@ -207,7 +252,9 @@ class GperftoolsConan(ConanFile):
 
         for component in self.cpp_info.components.values():
             if self.settings.os in ["Linux", "FreeBSD"]:
-                component.system_libs.extend(["pthread", "m"])
+                component.system_libs =["pthread", "m"]
+            elif self.settings.os == "Windows":
+                component.system_libs = ["psutil"]
             if self.options.get_safe("enable_libunwind"):
                 component.requires.append("libunwind::libunwind")
 
