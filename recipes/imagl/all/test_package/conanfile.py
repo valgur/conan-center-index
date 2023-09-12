6d129f86678149ef15db189badfa4c86c17665ef
Martin Valgur <martin.valgur@gmail.com>
2023-09-12 23:13:03 +0300

    imagl: merge changes from master

--- recipes/imagl/all/test_package/conanfile.py
+++ recipes/imagl/all/test_package/conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import cmake_layout, CMake
 import os
 
 
-class TestPackageConan(ConanFile):
+class ImaglTestConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
