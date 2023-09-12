c33314b300537fc1251a3377096c5795dbe2c9b4
Martin Valgur <martin.valgur@gmail.com>
2023-09-12 23:11:28 +0300

    antlr4-cppruntime: bump deps

--- recipes/antlr4-cppruntime/all/conanfile.py
+++ recipes/antlr4-cppruntime/all/conanfile.py
@@ -71,7 +71,7 @@ class Antlr4CppRuntimeConan(ConanFile):
         # Note that the above PR points that libuuid can be removed from 4.9.3, 4.10 and 4.10.1 as well.
         # We have patched the CMakeLists.txt to drop the dependency on libuuid from aforementioned antlr versions.
         if Version(self.version) < "4.10":
-            self.requires("utfcpp/3.2.3")
+            self.requires("utfcpp/3.2.4")
 
     def validate(self):
         # Compilation of this library on version 15 claims C2668 Error.
