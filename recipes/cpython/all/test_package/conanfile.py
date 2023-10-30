8fc1e7b5a303411d3decbc30014fb8104b5d25a7
Martin Valgur <martin.valgur@gmail.com>
2023-10-30 22:50:58 +0200

    cpython: WIP

--- recipes/cpython/all/test_package/conanfile.py
+++ recipes/cpython/all/test_package/conanfile.py
@@ -1,48 +1,19 @@
 import os
 import re
-import shutil
 from io import StringIO
 
 from conan import ConanFile
 from conan.errors import ConanException
 from conan.tools.apple import is_apple_os
-from conan.tools.build import cross_building
-from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.env import VirtualRunEnv
-from conan.tools.files import mkdir
+from conan.tools.build import cross_building, can_run
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.env import VirtualRunEnv, VirtualBuildEnv, Environment
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 
 
-class CmakePython3Abi(object):
-    def __init__(self, debug, pymalloc, unicode):
-        self.debug, self.pymalloc, self.unicode = debug, pymalloc, unicode
-
-    _cmake_lut = {
-        None: "ANY",
-        True: "ON",
-        False: "OFF",
-    }
-
-    @property
-    def suffix(self):
-        suffix = ""
-        if self.debug:
-            suffix += "d"
-        if self.pymalloc:
-            suffix += "m"
-        if self.unicode:
-            suffix += "u"
-        return suffix
-
-    @property
-    def cmake_arg(self):
-        return ";".join(self._cmake_lut[a] for a in (self.debug, self.pymalloc, self.unicode))
-
-
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps"
     test_type = "explicit"
 
     def requirements(self):
@@ -68,13 +39,6 @@ class TestPackageConan(ConanFile):
         return bool(self.dependencies["cpython"].options.get_safe("pymalloc", False))
 
     @property
-    def _cmake_abi(self):
-        if self._py_version < "3.8":
-            return CmakePython3Abi(debug=self.settings.build_type == "Debug", pymalloc=self._pymalloc, unicode=False)
-        else:
-            return CmakePython3Abi(debug=self.settings.build_type == "Debug", pymalloc=False, unicode=False)
-
-    @property
     def _cmake_try_FindPythonX(self):
         # FIXME: re-enable
         # return not is_msvc(self) or self.settings.build_type != "Debug"
@@ -85,70 +49,35 @@ class TestPackageConan(ConanFile):
         return not is_msvc(self) or self.dependencies["cpython"].options.shared
 
     def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+
         tc = CMakeToolchain(self)
-        version = self._py_version
-        py_major = str(version.major)
         tc.cache_variables["BUILD_MODULE"] = self._supports_modules
-        tc.cache_variables["PY_VERSION_MAJOR"] = py_major
-        tc.cache_variables["PY_VERSION_MAJOR_MINOR"] = f"{version.major}.{version.minor}"
-        tc.cache_variables["PY_FULL_VERSION"] = str(version)
-        tc.cache_variables["PY_VERSION"] = self._clean_py_version
-        tc.cache_variables["PY_VERSION_SUFFIX"] = self._cmake_abi.suffix
-        tc.cache_variables["PYTHON_EXECUTABLE"] = self._python
-        tc.cache_variables["USE_FINDPYTHON_X".format(py_major)] = self._cmake_try_FindPythonX
-        tc.cache_variables[f"Python{py_major}_EXECUTABLE"] = self._python
-        tc.cache_variables[f"Python{py_major}_ROOT_DIR"] = self.dependencies["cpython"].package_folder
-        tc.cache_variables[f"Python{py_major}_USE_STATIC_LIBS"] = not self.dependencies["cpython"].options.shared
-        tc.cache_variables[f"Python{py_major}_FIND_FRAMEWORK"] = "NEVER"
-        tc.cache_variables[f"Python{py_major}_FIND_REGISTRY"] = "NEVER"
-        tc.cache_variables[f"Python{py_major}_FIND_IMPLEMENTATIONS"] = "CPython"
-        tc.cache_variables[f"Python{py_major}_FIND_STRATEGY"] = "LOCATION"
-        if not is_msvc(self) and self._clean_py_version < "3.8":
-            tc.cache_variables[f"Python{py_major}_FIND_ABI"] = self._cmake_abi.cmake_arg
         tc.generate()
 
-        VirtualRunEnv(self).generate(scope="run")
-        VirtualRunEnv(self).generate(scope="build")
+        vb = VirtualBuildEnv(self)
+        vb.generate()
+
+        if can_run(self):
+            env = Environment()
+            env.define("DISTUTILS_USE_SDK", "1")
+            env.define("MSSdk", "1")
+
+            if self._cpython_option("with_curses"):
+                env.define("TERM", "ansi")
+            if is_apple_os(self) and not self.dependencies["cpython"].options.shared:
+                env.append_path("PYTHONPATH", self.build_folder)
+            env_vars = env.vars(self, scope="conanrun")
+            env_vars.save_script("modenv")
+
+            vr = VirtualRunEnv(self)
+            vr.generate()
 
     def build(self):
-        if not cross_building(self, skip_x64_x86=True):
-            command = f"{self._python} --version"
-            buffer = StringIO()
-            self.run(command, stdout=buffer, ignore_errors=True)
-            self.output.info(f"output: {buffer.getvalue()}")
-            self.run(command)
-
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-
-        if not cross_building(self, skip_x64_x86=True):
-            if self._supports_modules:
-                modsrcfolder = "py2" if self._py_version.major == 2 else "py3"
-                mkdir(self, os.path.join(self.build_folder, modsrcfolder))
-                for fn in os.listdir(os.path.join(self.source_folder, modsrcfolder)):
-                    shutil.copy(
-                        os.path.join(self.source_folder, modsrcfolder, fn),
-                        os.path.join(self.build_folder, modsrcfolder, fn),
-                    )
-                shutil.copy(
-                    os.path.join(self.source_folder, "setup.py"),
-                    os.path.join(self.build_folder, "setup.py"),
-                )
-                os.environ["DISTUTILS_USE_SDK"] = "1"
-                os.environ["MSSdk"] = "1"
-                setup_args = [
-                    f"{self.source_folder}/setup.py",
-                    # "conan",
-                    # "--install-folder", self.build_folder,
-                    "build",
-                    "--build-base", self.build_folder,
-                    "--build-platlib", os.path.join(self.build_folder, "lib_setuptools"),
-                ]
-                if self.settings.build_type == "Debug":
-                    setup_args.append("--debug")
-                args = " ".join(f'"{a}"' for a in setup_args)
-                self.run(f"{self._python} {args}")
 
     def _test_module(self, module, should_work):
         try:
