991d96051faa0ba8b2627b879b8b6c0c2e059fe2
Martin Valgur <martin.valgur@gmail.com>
2023-12-09 23:26:22 +0200

    pango: bump deps

--- recipes/pango/all/conanfile.py
+++ recipes/pango/all/conanfile.py
@@ -70,7 +70,7 @@ class PangoConan(ConanFile):
 
     def requirements(self):
         if self.options.with_freetype:
-            self.requires("freetype/2.13.0")
+            self.requires("freetype/2.13.2")
         if self.options.with_fontconfig:
             self.requires("fontconfig/2.14.2")
         if self.options.with_xft:
@@ -79,8 +79,8 @@ class PangoConan(ConanFile):
             self.requires("xorg/system")  # for xorg::xrender
         if self.options.with_cairo:
             self.requires("cairo/1.18.0", transitive_headers=True, transitive_libs=True)
-        self.requires("harfbuzz/8.2.2", transitive_headers=True, transitive_libs=True)
-        self.requires("glib/2.78.0", transitive_headers=True, transitive_libs=True)
+        self.requires("harfbuzz/8.3.0", transitive_headers=True, transitive_libs=True)
+        self.requires("glib/2.78.1", transitive_headers=True, transitive_libs=True)
         self.requires("fribidi/1.0.13")
 
     def validate(self):
@@ -105,8 +105,8 @@ class PangoConan(ConanFile):
 
     def build_requirements(self):
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
-            self.tool_requires("pkgconf/2.0.3")
-        self.tool_requires("meson/1.2.3")
+            self.tool_requires("pkgconf/2.1.0")
+        self.tool_requires("meson/1.3.0")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
