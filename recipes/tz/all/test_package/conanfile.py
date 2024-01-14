ae93356b225339086fde263ae24c6c207aac0359
Samuel Dowling <samuel.dowling@protonmail.com>
2024-01-14 10:41:59 +1030

    [tz] Refactor self.dependencies usage for conan 1.x compatibility
    Conan 1.x is not able to parse self.dependencies in the test() method
    due to differences in the lifetime of objects. The solution presented
    here is to save the necessary options to file and load them back again
    in the test() method to be used as appropriate.
    
    Additionally, utilise the conan `save` and `load` methods for
    readability and consistency with other recipes.
    
    Co-authored-by: Martin Valgur <martin.valgur@gmail.com>

--- recipes/tz/all/test_package/conanfile.py
+++ recipes/tz/all/test_package/conanfile.py
@@ -6,7 +6,6 @@ import os
 
 class TzTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    tzdata = None
 
     def layout(self):
         basic_layout(self, src_folder="src")
@@ -19,22 +18,17 @@ class TzTestConan(ConanFile):
         if self.settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", check_type=str):
             self.tool_requires("msys2/cci.latest")
 
-    def generate(self):
-        # INFO: zdump does not consume TZDATA, need to pass absolute path of the zoneinfo directory
-        self.tzdata = self.dependencies['tz'].runenv_info.vars(self).get('TZDATA')
-        with open("tzdata.info", "w") as fd:
-            fd.write(self.tzdata)
-
     def build(self):
         pass
 
     def test(self):
         if can_run(self):
-            if self.dependencies['tz'].options.with_binary_db:
+            tzdata = self.dependencies["tz"].runenv_info.vars(self).get("TZDATA")
+            with_binary_db = str(self.dependencies["tz"].options.with_binary_db)
+            if with_binary_db:
                 self.output.info("Test that binary tzdb is readable")
-                with open(os.path.join(self.generators_folder, "tzdata.info"), "r") as fd:
-                    self.tzdata = fd.read()
-                self.run(f"zdump {os.path.join(self.tzdata, 'America', 'Los_Angeles')}", env="conanrun")
+                la_tz = os.path.join(tzdata, "America", "Los_Angeles")
+                self.run(f"zdump {la_tz}", env="conanrun")
             else:
                 self.output.info("Test that source tzdb is readable")
                 cmd = "python -c \"import os; tzdata = os.environ['TZDATA']; f=open(os.path.join(tzdata, 'factory'), 'r'); s = f.read(); f.close(); print(s)\""
