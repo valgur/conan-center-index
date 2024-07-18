5f85cbac906d3bae8b9259f928633637b3df49c0
Martin Valgur <martin.valgur@gmail.com>
2024-07-18 20:42:54 +0300

    iridescence: update to cci.20240709

--- recipes/stella_vslam/all/conanfile.py
+++ recipes/stella_vslam/all/conanfile.py
@@ -85,7 +85,7 @@ class StellaVslamConan(ConanFile):
 
         # TODO: add support for viewers
         if self.options.build_iridescence_viewer:
-            self.requires("iridescence/cci.20240407")
+            self.requires("iridescence/cci.20240709")
         if self.options.build_pangolin_viewer:
             self.requires("pangolin/0.9.1")
         if self.options.build_socket_viewer:
