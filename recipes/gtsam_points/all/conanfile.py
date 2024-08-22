f70be1ba7c8960d5de3b8fb34e1d9859f6b80da8
Martin Valgur <martin.valgur@gmail.com>
2024-08-22 21:17:36 +0300

    gtsam_points: bump to v1.0.4

--- recipes/gtsam_points/all/conanfile.py
+++ recipes/gtsam_points/all/conanfile.py
@@ -100,7 +100,7 @@ class GtsamPointsPackage(ConanFile):
         cmake.build()
 
     def package(self):
-        # copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
