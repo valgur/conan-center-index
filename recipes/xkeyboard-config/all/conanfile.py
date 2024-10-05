80b412530784bf82d5c24cf823f7e7b85a1a880c
Martin Valgur <martin.valgur@gmail.com>
2024-10-05 18:30:51 +0300

    xkeyboard-config: add non-system recipe

--- recipes/xkeyboard-config/all/conanfile.py
+++ recipes/xkeyboard-config/all/conanfile.py
@@ -1,51 +1,81 @@
-from conan import ConanFile
-from conan.tools.gnu import PkgConfig
-from conan.tools.system import package_manager
-from conan.errors import ConanInvalidConfiguration
+import os
 
-required_conan_version = ">=1.50.0"
+from conan import ConanFile
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import copy, get, rmdir, replace_in_file
+from conan.tools.gnu import PkgConfigDeps
+from conan.tools.layout import basic_layout
+from conan.tools.meson import Meson, MesonToolchain
+
+required_conan_version = ">=1.53.0"
 
 
 class XkeyboardConfigConan(ConanFile):
     name = "xkeyboard-config"
-    package_type = "application"
+    description = "Keyboard configuration database for X11"
+    license = "HPND AND HPND-sell-variant AND X11 AND X11-distribute-modifications-variant AND MIT AND MIT-open-group AND xkeyboard-config-Zinoviev"
     url = "https://github.com/conan-io/conan-center-index"
-    license = "MIT"
-    homepage = "https://www.freedesktop.org/wiki/Software/XKeyboardConfig/"
-    description = "The non-arch keyboard configuration database for X Window."
-    settings = "os", "compiler", "build_type" # no arch here, because the xkeyboard-config system package is arch independant
-    topics = ("x11", "xorg", "keyboard")
+    homepage = "https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config"
+    topics = ("xorg", "x11", "keyboard")
 
-    def validate(self):
-        if self.settings.os not in ["Linux", "FreeBSD"]:
-            raise ConanInvalidConfiguration("This recipe supports only Linux and FreeBSD")
+    package_type = "application"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def package_id(self):
         self.info.clear()
 
-    def system_requirements(self):
-        apt = package_manager.Apt(self)
-        apt.install(["xkb-data"], update=True, check=True)
+    def requirements(self):
+        pass
 
-        yum = package_manager.Yum(self)
-        yum.install(["xkeyboard-config-devel"], update=True, check=True)
+    # def validate(self):
+    #     if is_msvc(self):
+    #         raise ConanInvalidConfiguration("MSVC is not supported.")
 
-        dnf = package_manager.Dnf(self)
-        dnf.install(["xkeyboard-config-devel"], update=True, check=True)
+    def build_requirements(self):
+        self.tool_requires("meson/[>=1.2.3 <2]")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/[>=2.2 <3]")
 
-        zypper = package_manager.Zypper(self)
-        zypper.install(["xkeyboard-config"], update=True, check=True)
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-        pacman = package_manager.PacMan(self)
-        pacman.install(["xkeyboard-config"], update=True, check=True)
+    def generate(self):
+        VirtualBuildEnv(self).generate()
+        tc = MesonToolchain(self)
+        tc.generate()
+        deps = PkgConfigDeps(self)
+        deps.generate()
 
-        package_manager.Pkg(self).install(["xkeyboard-config"], update=True, check=True)
+    def _patch_sources(self):
+        replace_in_file(self, os.path.join(self.source_folder, "meson.build"),
+                        "enable_pytest = ", "enable_pytest = false #")
+
+    def build(self):
+        self._patch_sources()
+        meson = Meson(self)
+        meson.configure()
+        meson.build()
+
+    def package(self):
+        copy(self, "COPYING", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        meson = Meson(self)
+        meson.install()
+        rmdir(self, os.path.join(self.package_folder, "share", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share", "man"))
+        os.rename(os.path.join(self.package_folder, "share"),
+                  os.path.join(self.package_folder, "res"))
 
     def package_info(self):
-        pkg_config = PkgConfig(self, "xkeyboard-config")
-        pkg_config.fill_cpp_info(
-            self.cpp_info, is_system=self.settings.os != "FreeBSD")
         self.cpp_info.set_property("pkg_config_name", "xkeyboard-config")
-        self.cpp_info.set_property("component_version", pkg_config.version)
-        self.cpp_info.set_property("pkg_config_custom_content",
-                                                    "\n".join(f"{key}={value}" for key, value in pkg_config.variables.items() if key not in ["pcfiledir","prefix", "includedir"]))
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.includedirs = []
+        self.cpp_info.resdirs = ["res"]
+
+        self.cpp_info.set_property("pkg_config_custom_content", "\n".join([
+            "datadir=${prefix}/res",
+            "xkb_base=${datadir}/X11/xkb"
+        ]))
--- recipes/xkeyboard-config/all/test_package/conanfile.py
+++ recipes/xkeyboard-config/all/test_package/conanfile.py
@@ -1,23 +1,31 @@
+import os.path
+
 from conan import ConanFile
-from conan.tools.gnu import PkgConfigDeps
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.gnu import PkgConfig
 from conan.tools.layout import basic_layout
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "VirtualBuildEnv", "VirtualRunEnv"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "AutotoolsToolchain", "PkgConfigDeps"
     test_type = "explicit"
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
 
     def layout(self):
         basic_layout(self)
 
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def build_requirements(self):
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/[>=2.2 <3]")
+
     def generate(self):
-        pkg_config_deps = PkgConfigDeps(self)
-        pkg_config_deps.generate()
+        VirtualBuildEnv(self).generate()
+        VirtualBuildEnv(self).generate(scope="run")
 
     def test(self):
-        pkg_config = self.conf_info.get("tools.gnu:pkg_config", default="pkg-config")
-        self.run(f"{pkg_config} --validate xkeyboard-config")
+        xkb_base = PkgConfig(self, "xkeyboard-config").variables["xkb_base"]
+        self.output.info(f"xkb_base path: {xkb_base}")
+        assert os.path.isdir(xkb_base)
