534b116ff6f2b03102530eb83ad9827b7cb8c042
Martin Valgur <martin.valgur@gmail.com>
2024-10-15 01:44:53 +0300

    insicators: add a missing include

--- recipes/indicators/all/test_package/test_package.cpp
+++ recipes/indicators/all/test_package/test_package.cpp
@@ -1,3 +1,6 @@
+// indicators/termcolor.hpp is missing #include <cstdint>
+#include <cstdint>
+
 #include <indicators/cursor_control.hpp>
 #include <indicators/progress_bar.hpp>
 #include <indicators/progress_spinner.hpp>
