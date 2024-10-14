f219a434151a1c392a6c4c4471cab9359d331200
Martin Valgur <martin.valgur@gmail.com>
2024-10-14 23:24:40 +0300

    flatbush: fix test_package when using C++20

--- recipes/flatbush/all/test_package/conanfile.py
+++ recipes/flatbush/all/test_package/conanfile.py
@@ -1,12 +1,13 @@
-from conan import ConanFile
-from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
 import os
+
+from conan import ConanFile
+from conan.tools.build import can_run, valid_min_cppstd
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps"
     test_type = "explicit"
 
     def requirements(self):
@@ -14,6 +15,11 @@ class TestPackageConan(ConanFile):
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["USE_CXX20"] = valid_min_cppstd(self, "20")
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
