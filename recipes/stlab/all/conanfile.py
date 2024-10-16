b0a8b47fd16345a82c502f218a19c3e14bf247c0
Martin Valgur <martin.valgur@gmail.com>
2024-10-16 18:16:42 +0300

    stlab: add cross-compilation support

--- recipes/stlab/all/conanfile.py
+++ recipes/stlab/all/conanfile.py
@@ -2,22 +2,21 @@ import os
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd, cross_building
+from conan.tools.build import check_min_cppstd, can_run, valid_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rm, rmdir
-from conan.tools.microsoft import check_min_vs, is_msvc
 from conan.tools.scm import Version
 
 required_conan_version = ">=1.52.0"
 
 
 class Stlab(ConanFile):
-    name = 'stlab'
-    description = 'The Software Technology Lab libraries.'
-    url = 'https://github.com/conan-io/conan-center-index'
-    homepage = 'https://github.com/stlab/libraries'
-    license = 'BSL-1.0'
-    topics = 'concurrency', 'futures', 'channels'
+    name = "stlab"
+    description = "The Software Technology Lab libraries."
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/stlab/libraries"
+    license = "BSL-1.0"
+    topics = "concurrency", "futures", "channels"
     settings = "arch", "os", "compiler", "build_type",
     options = {
         "with_boost": [True, False],
@@ -40,7 +39,7 @@ class Stlab(ConanFile):
         self.options.thread_system = {"Macos": "pthread-apple",
                                       "Linux": "pthread",
                                       "Windows": "win32",
-                                      "Emscripten": "pthread-emscripten"}.get(str(self.settings.os), "none")
+                                      "Emscripten": "pthread-emscripten",}.get(str(self.settings.os), "none")
         self.options.task_system = {"Macos": "libdispatch",
                                     "Windows": "windows"}.get(str(self.settings.os), "portable")
 
@@ -50,9 +49,13 @@ class Stlab(ConanFile):
 
     @property
     def _compilers_minimum_version(self):
-        return {"gcc": "9",
-                "clang": "8",
-                "apple-clang": "13"}
+        return {
+            "gcc": "9",
+            "clang": "8",
+            "apple-clang": "13",
+            "msvc": "192",
+            "Visual Studio": "16",
+        }
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -63,9 +66,8 @@ class Stlab(ConanFile):
     def requirements(self):
         if self.options.with_boost:
             self.requires("boost/1.85.0")
-
-        # On macOS, it is not necessary to use the libdispatch conan package, because the library is
-        # included in the OS.
+        # On macOS, it is not necessary to use the libdispatch conan package,
+        # because the library is included in the OS.
         if self.options.task_system == "libdispatch" and self.settings.os != "Macos":
             self.requires("libdispatch/5.3.2")
 
@@ -99,20 +101,15 @@ class Stlab(ConanFile):
                 f"so we will use boost::optional and boost::variant instead. Try -o {self.ref}:with_boost=True.")
 
     def _validate_min_compiler_version(self):
-        if is_msvc(self):
-            check_min_vs(self, "192")
-        else:
-            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-            if not minimum_version:
-                self.output.warn(f"{self.ref} requires C++{self._minimum_cpp_standard}. "
-                                 f"Your compiler is unknown. Assuming it supports C++{self._minimum_cpp_standard}.")
-            elif Version(str(self.settings.compiler.version)) < minimum_version:
-                raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._minimum_cpp_standard}, "
-                                                f"which your compiler does not support.")
-            if self.settings.compiler == "clang" and str(self.settings.compiler.version) in ("13", "14"):
-                raise ConanInvalidConfiguration(
-                    f"{self.ref} currently does not work with Clang {self.settings.compiler.version} on CCI, it enters "
-                    f"in an infinite build loop (smells like a compiler bug). Contributions are welcomed!")
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(str(self.settings.compiler.version)) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._minimum_cpp_standard}, which your compiler does not support."
+            )
+        if self.settings.compiler == "clang" and str(self.settings.compiler.version) in ("13", "14"):
+            raise ConanInvalidConfiguration(
+                f"{self.ref} currently does not work with Clang {self.settings.compiler.version} on CCI, it enters "
+                f"in an infinite build loop (smells like a compiler bug). Contributions are welcomed!")
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
@@ -124,21 +121,38 @@ class Stlab(ConanFile):
         self._validate_boost_components()
 
     def configure(self):
-        self.output.info("STLab With Boost: {}.".format(self.options.with_boost))
-        self.output.info("STLab Future Coroutines: {}.".format(self.options.future_coroutines))
-        self.output.info("STLab No Standard Coroutines: {}.".format(self.options.no_std_coroutines))
-        self.output.info("STLab Task System: {}.".format(self.options.task_system))
-        self.output.info("STLab Thread System: {}.".format(self.options.thread_system))
+        self.output.info(f"STLab With Boost: {self.options.with_boost}.")
+        self.output.info(f"STLab Future Coroutines: {self.options.future_coroutines}.")
+        self.output.info(f"STLab No Standard Coroutines: {self.options.no_std_coroutines}.")
+        self.output.info(f"STLab Task System: {self.options.task_system}.")
+        self.output.info(f"STLab Thread System: {self.options.thread_system}.")
+
+    @property
+    def _has_coroutines_support(self):
+        if self.settings.compiler.cppstd and not valid_min_cppstd(self, 20):
+            return False
+        min_ver = {
+            "gcc": "10",
+            "clang": "13",
+            "apple-clang": "13",
+            "msvc": "192",
+            "Visual Studio": "16",
+        }.get(str(self.settings.compiler))
+        return min_ver and Version(self.settings.compiler.version) >= min_ver
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables['BUILD_TESTING'] = not self.conf.get("tools.build:skip_test", default=True, check_type=bool)
+        tc.variables["BUILD_TESTING"] = False
         tc.variables["STLAB_USE_BOOST_CPP17_SHIMS"] = self.options.with_boost
         tc.variables["STLAB_NO_STD_COROUTINES"] = self.options.no_std_coroutines
         tc.variables["STLAB_THREAD_SYSTEM"] = self.options.thread_system
         tc.variables["STLAB_TASK_SYSTEM"] = self.options.task_system
-        if cross_building(self):
+        tc.variables["CMAKE_REQUIRE_FIND_PACKAGE_Boost"] = self.options.with_boost
+        tc.variables["CMAKE_REQUIRE_FIND_PACKAGE_libdispatch"] = self.options.task_system == "libdispatch"
+        tc.variables["CMAKE_DISABLE_FIND_PACKAGE_Qt6"] = True
+        if not can_run(self):
             tc.variables["STLAB_HAVE_FUNCTIONAL_VARIANT_OPTIONAL"] = True
+            tc.variables["STLAB_HAVE_FUNCTIONAL_COROUTINES"] = self._has_coroutines_support
         tc.generate()
 
         tc = CMakeDeps(self)
@@ -150,30 +164,21 @@ class Stlab(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
-
         rmdir(self, os.path.join(self.package_folder, "share"))
         rm(self, "msvcp*.dll", os.path.join(self.package_folder, "bin"))
         rm(self, "concrt*.dll", os.path.join(self.package_folder, "bin"))
         rm(self, "vcruntime*.dll", os.path.join(self.package_folder, "bin"))
 
     def package_id(self):
-        # TODO: is header only but needs a header modified by cmake
-        # self.info.settings.clear()
-        # self.info.header_only()
-        pass
+        self.info.settings.clear()
 
     def package_info(self):
         future_coroutines_value = 1 if self.options.future_coroutines else 0
-
-        self.cpp_info.defines = [
-            'STLAB_FUTURE_COROUTINES={}'.format(future_coroutines_value)
-        ]
-
+        self.cpp_info.defines.append(f"STLAB_FUTURE_COROUTINES={future_coroutines_value}")
         if self.settings.os == "Windows":
-            self.cpp_info.defines = ['NOMINMAX']
-
+            self.cpp_info.defines.append("NOMINMAX")
         if self.settings.os == "Linux":
             self.cpp_info.system_libs = ["pthread"]
