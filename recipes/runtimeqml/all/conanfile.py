b37b15eb0d94cb57bd46ec5e0c30f0714da6a4cc
Martin Valgur <martin.valgur@gmail.com>
2024-11-11 16:46:46 +0200

    runtimeqml: fix cross-compilation

--- recipes/runtimeqml/all/conanfile.py
+++ recipes/runtimeqml/all/conanfile.py
@@ -1,13 +1,13 @@
-from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
-from conan.tools.microsoft import is_msvc, check_min_vs
-from conan.tools.build import check_min_cppstd
-from conan.tools.scm import Version
-from conan.tools.files import get, copy
-from conan.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.50.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd, can_run
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.files import get, copy
+from conan.tools.scm import Version
+
+required_conan_version = ">=2.0.9"
 
 
 class RuntimeQml(ConanFile):
@@ -27,54 +27,27 @@ class RuntimeQml(ConanFile):
         "shared": False,
         "fPIC": True
     }
-
-    @property
-    def _minimum_cpp_standard(self):
-        return 17
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "gcc": "7",
-            "clang": "7",
-            "apple-clang": "10",
-        }
+    implements = ["auto_shared_fpic"]
 
     def export_sources(self):
         copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=os.path.join(self.export_sources_folder, "src"))
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
     def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
+        self.options["qt"].qtdeclarative = True
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
         if Version(self.version) <= "cci.20211220":
-            self.requires("qt/[~5.15]")
+            self.requires("qt/[~5.15]", run=can_run(self))
         else:
-            self.requires("qt/[>=6.6 <7]")
+            self.requires("qt/[>=6.6 <7]", run=can_run(self))
 
     def validate(self):
-        if self.info.settings.compiler.cppstd:
-            check_min_cppstd(self, self._minimum_cpp_standard)
-        check_min_vs(self, 191)
-        if not is_msvc(self):
-            minimum_version = self._compilers_minimum_version.get(
-                str(self.info.settings.compiler), False)
-            if minimum_version and Version(self.info.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration(
-                    f"{self.ref} requires C++{self._minimum_cpp_standard}, which your compiler does not support."
-                )
-        qt = self.dependencies["qt"]
-        if not qt.options.qtdeclarative:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires option qt:qtdeclarative=True")
+        check_min_cppstd(self, 17)
+        if not self.dependencies["qt"].options.qtdeclarative:
+            raise ConanInvalidConfiguration(f"{self.ref} requires option qt:qtdeclarative=True")
 
     def source(self):
         get(self, **self.conan_data["sources"][str(self.version)], strip_root=True)
@@ -82,8 +55,8 @@ class RuntimeQml(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.generate()
-        tc = CMakeDeps(self)
-        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
