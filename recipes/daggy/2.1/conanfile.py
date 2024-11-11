0f4e7a8a9e97074361f4fa6bce26b7ec56f5a8d2
Martin Valgur <martin.valgur@gmail.com>
2024-11-11 17:38:33 +0200

    daggy: fix cross-compilation

--- recipes/daggy/2.1/conanfile.py
+++ recipes/daggy/2.1/conanfile.py
@@ -2,13 +2,11 @@ import os
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd
+from conan.tools.build import check_min_cppstd, can_run
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
 from conan.tools.files import copy, get, export_conandata_patches, apply_conandata_patches
-from conan.tools.scm import Version
 
-required_conan_version = ">=1.60.0 <2 || >=2.0.5"
+required_conan_version = ">=2.0.9"
 
 
 class DaggyConan(ConanFile):
@@ -19,7 +17,6 @@ class DaggyConan(ConanFile):
     homepage = "https://github.com/synacker/daggy"
     topics = ("streaming", "qt", "monitoring", "process", "stream-processing", "extensible",
               "serverless-framework", "aggregation", "ssh2", "cross-platform", "ssh-client")
-
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -36,39 +33,17 @@ class DaggyConan(ConanFile):
         "with_yaml": True,
         "with_console": False,
     }
-
-    @property
-    def _min_cppstd(self):
-        return 17
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "Visual Studio": "16",
-            "msvc": "192",
-            "gcc": "8",
-            "clang": "8",
-            "apple-clang": "10",
-        }
+    implements = ["auto_shared_fpic"]
 
     def export_sources(self):
         export_conandata_patches(self)
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-        self.options["qt"].shared = True
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
         # Qt is used in the public headers
-        self.requires("qt/[>=6.7 <7]", transitive_headers=True, transitive_libs=True)
+        self.requires("qt/[>=6.7 <7]", transitive_headers=True, transitive_libs=True, run=can_run(self))
         self.requires("kainjow-mustache/4.1")
         if self.options.with_yaml:
             self.requires("yaml-cpp/0.8.0")
@@ -76,16 +51,7 @@ class DaggyConan(ConanFile):
             self.requires("libssh2/1.11.0")
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
-        if not self.dependencies["qt"].options.shared:
-            raise ConanInvalidConfiguration("Shared Qt lib is required.")
-
+        check_min_cppstd(self, 17)
         # TODO: can be removed after https://github.com/conan-io/conan-center-index/pull/23683 is merged
         if self.options.with_ssh2 and self.settings.os == "Windows":
             if self.options.shared != self.dependencies["libssh2"].options.shared:
@@ -93,19 +59,14 @@ class DaggyConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.21 <4]")
-        self.tool_requires("qt/<host_version>")
+        if not can_run(self):
+            self.tool_requires("qt/<host_version>")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
-        env = VirtualBuildEnv(self)
-        env.generate()
-
-        # Required for Qt's moc
-        env = VirtualRunEnv(self)
-        env.generate(scope="build")
-
         tc = CMakeToolchain(self)
         tc.variables["SSH2_SUPPORT"] = self.options.with_ssh2
         tc.variables["YAML_SUPPORT"] = self.options.with_yaml
@@ -123,7 +84,6 @@ class DaggyConan(ConanFile):
         tc.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure(build_script_folder=os.path.join(self.source_folder, "src"))
         cmake.build()
