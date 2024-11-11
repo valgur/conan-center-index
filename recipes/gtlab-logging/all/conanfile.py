49b19822885b964157747d80c5786fe9f2655e6b
Martin Valgur <martin.valgur@gmail.com>
2024-11-11 18:06:44 +0200

    gtlab-logging: fix Qt6 compilation errors, drop Conan v1 compatibility

--- recipes/gtlab-logging/all/conanfile.py
+++ recipes/gtlab-logging/all/conanfile.py
@@ -1,21 +1,20 @@
+import os
 from pathlib import Path
 
-from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
-from conan.tools import files
-from conan.tools.build import check_min_cppstd, check_max_cppstd
-from conan.errors import ConanInvalidConfiguration
 from conan import ConanFile
-import os
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.files import get, rmdir, copy, export_conandata_patches, apply_conandata_patches
 
-required_conan_version = ">=1.59.0"
+required_conan_version = ">=2.0.9"
 
 class GTLabLoggingConan(ConanFile):
     name = "gtlab-logging"
+    description = "Simple logging interface with Qt support"
     license = "BSD-3-Clause"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/dlr-gtlab/gt-logging"
     topics = ("logging", "qt")
-    description = "Simple logging interface with qt support"
 
     settings = "os", "arch", "compiler", "build_type"
     package_type = "library"
@@ -23,54 +22,25 @@ class GTLabLoggingConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "with_qt": [True, False]
+        "with_qt": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "with_qt": False
+        "with_qt": False,
     }
+
+    implements = ["auto_shared_fpic"]
+
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def requirements(self):
         if self.options.with_qt:
-            self.requires("qt/[>=5.15 <7]", transitive_headers=True)
-
-    @property
-    def _min_cppstd(self):
-        return "14"
-
-    @property
-    def _max_cppstd(self):
-        return "17"
-
-    @property
-    def _minimum_compilers_version(self):
-        return {
-            "14": {
-                "Visual Studio": "15",
-                "msvc": "191",
-                "gcc": "7.3.1",
-                "clang": "6",
-                "apple-clang": "14",
-            },
-        }.get(self._min_cppstd, {})
+            self.requires("qt/[>=5.15 <7]", transitive_headers=True, transitive_libs=True)
 
     def validate(self):
-        if self.settings.get_safe("compiler.cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
-            # C++20 fails with
-            # error: use of deleted function ‘std::basic_ostream<char, _Traits>& std::operator<<(basic_ostream<char, _Traits>&, char32_t)
-            check_max_cppstd(self, self._max_cppstd)
-
-        def loose_lt_semver(v1, v2):
-            return all(int(p1) < int(p2) for p1, p2 in zip(str(v1).split("."), str(v2).split(".")))
-
-        compiler = self.settings.compiler
-        min_version = self._minimum_compilers_version.get(str(compiler))
-        if min_version and loose_lt_semver(str(compiler.version), min_version):
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 14)
 
     def generate(self):
         CMakeToolchain(self).generate()
@@ -79,43 +49,26 @@ class GTLabLoggingConan(ConanFile):
     def layout(self):
         cmake_layout(self, src_folder="src")
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-
     def source(self):
-        files.get(self, **self.conan_data["sources"][self.version], strip_root=True)
-
-    def _patch_sources(self):
-        path = Path(self.source_folder, "src", "gt_logstream.h")
-        path.write_text("#include <cstdint>\n" + path.read_text())
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def package(self):
+        copy(self, "BSD-3-Clause.txt", os.path.join(self.source_folder, "LICENSES"), os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
-
-        files.rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
-        files.copy(self, os.path.join("LICENSES", "BSD-3-Clause.txt"), dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.libs = ["GTlabLogging"] if self.settings.build_type != "Debug" else ["GTlabLogging-d"]
-
-        self.cpp_info.includedirs.append(os.path.join("include", "logging"))
-
-        self.cpp_info.libdirs = [os.path.join("lib", "logging")]
-
         self.cpp_info.set_property("cmake_file_name", "GTlabLogging")
         self.cpp_info.set_property("cmake_target_name", "GTlab::Logging")
-
+        self.cpp_info.libs = ["GTlabLogging"] if self.settings.build_type != "Debug" else ["GTlabLogging-d"]
+        self.cpp_info.includedirs.append(os.path.join("include", "logging"))
+        self.cpp_info.libdirs = [os.path.join("lib", "logging")]
         if self.options.with_qt:
-            self.cpp_info.defines = ['GT_LOG_USE_QT_BINDINGS']
+            self.cpp_info.defines = ["GT_LOG_USE_QT_BINDINGS"]
