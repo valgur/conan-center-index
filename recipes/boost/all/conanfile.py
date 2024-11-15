d0437435aa2f286fe55bb5b8d9bf8bfd2a567dc3
Martin Valgur <martin.valgur@gmail.com>
2024-11-15 21:14:11 +0200

    boost: fix cppstd flag handling

--- recipes/boost/all/conanfile.py
+++ recipes/boost/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanException, ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os, to_apple_arch, XCRun
-from conan.tools.build import build_jobs, cross_building, valid_min_cppstd, can_run
+from conan.tools.build import build_jobs, cross_building, valid_min_cppstd, can_run, cppstd_flag
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import (
     apply_conandata_patches, chdir, collect_libs, copy, export_conandata_patches,
@@ -22,7 +22,7 @@ import sys
 from pathlib import Path
 import yaml
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.2.0"
 
 # When adding (or removing) an option, also add this option to the list in
 # `rebuild-dependencies.yml` and re-run that script.
@@ -162,64 +162,6 @@ class BoostConan(ConanFile):
 
     def export_sources(self):
         export_conandata_patches(self)
-
-    def _cppstd_flag(self):
-        """Return the flag for the given C++ standard and compiler"""
-        # TODO: Replace it by Conan tool when available: https://github.com/conan-io/conan/issues/12603
-        compiler = self.settings.compiler
-        compiler_version = self.settings.compiler.version
-        cppstd = self.settings.compiler.cppstd
-
-        def _cppstd_gcc(gcc_version, cppstd):
-            """Return the flag for the given C++ standard and GCC version"""
-            cppstd_flags = {}
-            cppstd_flags.setdefault("98", "98" if gcc_version >= "3.4" else None)
-            cppstd_flags.setdefault("11", "11" if gcc_version >= "4.7" else "0x" if gcc_version >= "4.3" else None)
-            cppstd_flags.setdefault("14", "14" if gcc_version >= "4.9" else "1y" if gcc_version >= "4.8" else None)
-            cppstd_flags.setdefault("17", "17" if gcc_version >= "5.2" else "1z" if gcc_version >= "5" else None)
-            cppstd_flags.setdefault("20", "2a" if gcc_version >= "8" else "20" if gcc_version >= "12" else None)
-            cppstd_flags.setdefault("23", "2b" if gcc_version >= "11" else None)
-            return cppstd_flags.get(cppstd.lstrip("gnu"))
-
-        def _cppstd_clang(clang_version, cppstd):
-            """Return the flag for the given C++ standard and Clang version"""
-            cppstd_flags = {}
-            cppstd_flags.setdefault("98", "98" if clang_version >= "2.1" else None)
-            cppstd_flags.setdefault("11", "11" if clang_version >= "3.1" else "0x" if clang_version >= "2.1" else None)
-            cppstd_flags.setdefault("14", "14" if clang_version >= "3.5" else "1y" if clang_version >= "3.4" else None)
-            cppstd_flags.setdefault("17", "17" if clang_version >= "5" else "1z" if clang_version >= "3.5" else None)
-            cppstd_flags.setdefault("20", "2a" if clang_version >= "6" else "20" if clang_version >= "12" else None)
-            cppstd_flags.setdefault("23", "2b" if clang_version >= "13"  else "23" if clang_version >= "17" else None)
-            return cppstd_flags.get(cppstd.lstrip("gnu"))
-
-
-        def _cppstd_apple_clang(clang_version, cppstd):
-            """Return the flag for the given C++ standard and Apple Clang version"""
-            cppstd_flags = {}
-            cppstd_flags.setdefault("98", "98" if clang_version >= "4.0" else None)
-            cppstd_flags.setdefault("11", "11" if clang_version >= "4.0" else None)
-            cppstd_flags.setdefault("14", "14" if clang_version >= "6.1" else "1y" if clang_version >= "5.1" else None)
-            cppstd_flags.setdefault("17", "17" if clang_version >= "9.1" else "1z" if clang_version >= "6.1" else None)
-            cppstd_flags.setdefault("20", "20" if clang_version >= "13.0" else "2a" if clang_version >= "10.0" else None)
-            cppstd_flags.setdefault("23", "2b" if clang_version >= "13.0" else None)
-            return cppstd_flags.get(cppstd.lstrip("gnu"))
-
-        def _cppstd_msvc(visual_version, cppstd):
-            """Return the flag for the given C++ standard and MSVC version"""
-            cppstd_flags = {}
-            cppstd_flags.setdefault("98", "98")
-            cppstd_flags.setdefault("11", "11")
-            cppstd_flags.setdefault("14", "14" if visual_version >= "190" else None)
-            cppstd_flags.setdefault("17", "17" if visual_version >= "191" else "latest" if visual_version >= "190" else None)
-            cppstd_flags.setdefault("20", "20" if visual_version >= "192" else "latest" if visual_version >= "191" else None)
-            cppstd_flags.setdefault("23", "latest" if visual_version >= "193" else None)
-            return cppstd_flags.get(cppstd)
-
-        func = {"gcc": _cppstd_gcc, "clang": _cppstd_clang, "apple-clang": _cppstd_apple_clang, "msvc": _cppstd_msvc}.get(compiler)
-        flag = cppstd
-        if func:
-            flag = func(Version(compiler_version), str(cppstd))
-        return flag
 
     @property
     def _min_compiler_version_default_cxx11(self):
@@ -1203,18 +1145,10 @@ class BoostConan(ConanFile):
 
         flags.append(f"toolset={self._toolset}")
 
-        safe_cppstd = self.settings.get_safe("compiler.cppstd")
-        if safe_cppstd:
-            cppstd_version = self._cppstd_flag()
-            flags.append(f"cxxstd={cppstd_version}")
-            if "gnu" in safe_cppstd:
-                flags.append("cxxstd-dialect=gnu")
-        elif Version(self.version) >= "1.85.0" and valid_min_cppstd(self, 14):
-            cppstd_version = self._cppstd_flag()
-            flags.append(f"cxxstd={cppstd_version}")
-        elif valid_min_cppstd(self, 11):
-            cppstd_version = self._cppstd_flag()
-            flags.append(f"cxxstd={cppstd_version}")
+        cxxstd = cppstd_flag(self)[-2:]
+        flags.append(f"cxxstd={cxxstd}")
+        if "gnu" in self.settings.compiler.cppstd:
+            flags.append("cxxstd-dialect=gnu")
 
         # LDFLAGS
         link_flags = []
