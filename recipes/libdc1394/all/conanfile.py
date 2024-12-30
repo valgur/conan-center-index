64df78d0afdcc2540a06fbd12e4a7741e11f313c
Martin Valgur <martin.valgur@gmail.com>
2024-12-30 11:43:41 +0200

    libdc1394: disable auto-detection of deps

--- recipes/libdc1394/all/conanfile.py
+++ recipes/libdc1394/all/conanfile.py
@@ -57,14 +57,16 @@ class Libdc1394Conan(ConanFile):
             self.tool_requires("pkgconf/[>=2.2 <3]")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         env = VirtualBuildEnv(self)
         env.generate()
         tc = AutotoolsToolchain(self)
         tc.configure_args.append("--disable-examples")
+        # Disable auto-detection of X11 and SDL dependencies
+        tc.configure_args.append("--without-x")
+        tc.configure_args.append("SDL_CONFIG=/dev/null")
         tc.generate()
         deps = PkgConfigDeps(self)
         deps.generate()
