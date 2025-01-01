39904dc9148f2d8decd756f7d412f801c2800ca9
Martin Valgur <martin.valgur@gmail.com>
2025-01-01 19:09:27 +0200

    libv4l: add gettext dep for consistent i18n handling

--- recipes/libv4l/all/conanfile.py
+++ recipes/libv4l/all/conanfile.py
@@ -69,6 +69,7 @@ class Libv4lConan(ConanFile):
         self.tool_requires("meson/[>=1.2.3 <2]")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/[>=2.2 <3]")
+        self.tool_requires("gettext/0.22.5")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
