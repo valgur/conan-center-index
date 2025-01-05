774964413b6c82aa9ddb2024152c4803b05413e6
sascha <s.lumma@gmail.com>
2025-01-05 14:54:03 +0100

    Add required explicit version sepcification

--- recipes/poppler/all/conanfile.py
+++ recipes/poppler/all/conanfile.py
@@ -14,6 +14,7 @@ required_conan_version = ">=1.56.0 <2 || >=2.0.6"
 
 class PopplerConan(ConanFile):
     name = "poppler"
+    version = "24.05.0"
     description = "Poppler is a PDF rendering library based on the xpdf-3.0 code base"
     license = ("GPL-2.0-or-later", "GPL-3.0-or-later")
     url = "https://github.com/conan-io/conan-center-index"
