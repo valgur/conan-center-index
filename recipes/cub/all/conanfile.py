617d2e43b2b71e2cb0cefaef865e699683008522
Martin Valgur <martin.valgur@gmail.com>
2025-01-10 14:38:37 +0200

    cuda-cccl: update to 2.7.0

--- recipes/cub/all/conanfile.py
+++ recipes/cub/all/conanfile.py
@@ -1,7 +1,6 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get, move_folder_contents
 from conan.tools.layout import basic_layout
@@ -22,19 +21,6 @@ class CubConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    @property
-    def _min_cppstd(self):
-        return 14
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "msvc": "191",
-            "gcc": "5",
-            "clang": "5",
-            "apple-clang": "5.1",
-        }
-
     def layout(self):
         basic_layout(self, src_folder="src")
 
@@ -46,12 +32,7 @@ class CubConan(ConanFile):
             self.requires(f"libcudacxx/{self.version}")
 
     def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 14)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
