7a06aefce0f6713628b813d9ee5452b8dd0810b4
Martin Valgur <martin.valgur@gmail.com>
2025-01-10 10:16:19 +0200

    libcaca: fix stray _settings_build

--- recipes/libcaca/all/conanfile.py
+++ recipes/libcaca/all/conanfile.py
@@ -45,10 +45,6 @@ class LibcacaConan(ConanFile):
         "with_x11": False,
     }
 
-    @property
-    def _settings_build(self):
-        return getattr(self, "settings_build", self.settings)
-
     def export_sources(self):
         export_conandata_patches(self)
 
