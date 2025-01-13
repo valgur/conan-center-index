456172ba08b6b0a7254e6c172df0a690137b24ee
Martin Valgur <martin.valgur@gmail.com>
2025-01-13 10:33:39 +0200

    opentelemetry-proto: remove Conan v1 workaround from test_package

--- recipes/opentelemetry-proto/all/test_package/conanfile.py
+++ recipes/opentelemetry-proto/all/test_package/conanfile.py
@@ -1,7 +1,6 @@
 import os
 
 from conan import ConanFile
-from conan.tools.files import save, load
 from conan.tools.layout import basic_layout
 
 
@@ -14,10 +13,7 @@ class TestPackageConan(ConanFile):
     def requirements(self):
         self.requires(self.tested_reference_str)
 
-    def generate(self):
-        save(self, os.path.join(self.build_folder, "proto_root"), self.dependencies["opentelemetry-proto"].conf_info.get("user.opentelemetry-proto:proto_root"))
-
     def test(self):
-        res_folder = load(self, os.path.join(self.build_folder, "proto_root"))
+        res_folder = self.dependencies["opentelemetry-proto"].conf_info.get("user.opentelemetry-proto:proto_root")
         proto_path = os.path.join(res_folder, "opentelemetry", "proto", "common", "v1", "common.proto")
         assert os.path.isfile(proto_path)
