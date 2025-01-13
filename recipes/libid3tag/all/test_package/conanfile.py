81fe9fa760d6bb6b10a4f291e1bd7dcd432ad27d
Martin Valgur <martin.valgur@gmail.com>
2025-01-13 22:27:50 +0200

    libid3tag: fix test_package

--- recipes/libid3tag/all/test_package/conanfile.py
+++ recipes/libid3tag/all/test_package/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
+from conan.tools.cmake import cmake_layout, CMake
 import os
 
 
@@ -13,10 +13,6 @@ class TestPackageConan(ConanFile):
 
     def layout(self):
         cmake_layout(self)
-
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.generate()
 
     def build(self):
         cmake = CMake(self)
