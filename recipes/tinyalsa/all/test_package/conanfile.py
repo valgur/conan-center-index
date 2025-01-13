b33fd5a78d9a46c97cf9d1bdea06295031be190c
Martin Valgur <martin.valgur@gmail.com>
2025-01-13 10:23:59 +0200

    tinyalsa: remove Conan v1 workaround from test_package

--- recipes/tinyalsa/all/test_package/conanfile.py
+++ recipes/tinyalsa/all/test_package/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
-from conan.tools.files import load, save
 import os
 
 
@@ -15,10 +14,6 @@ class TestPackageConan(ConanFile):
     def layout(self):
         cmake_layout(self)
 
-    def generate(self):
-        save(self, os.path.join(self.build_folder, "with_utils"),
-             str(self.dependencies["tinyalsa"].options.with_utils))
-
     def build(self):
         cmake = CMake(self)
         cmake.configure()
@@ -28,5 +23,5 @@ class TestPackageConan(ConanFile):
         if can_run(self):
             bin_path = os.path.join(self.cpp.build.bindir, "example")
             self.run(bin_path, env="conanrun")
-            if load(self, os.path.join(self.build_folder, "with_utils")) == "True":
+            if self.dependencies["tinyalsa"].options.with_utils:
                 self.run("tinymix --help", env="conanrun")
