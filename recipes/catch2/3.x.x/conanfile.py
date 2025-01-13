d817a81a197f91178269ba378f66fceaf47d6f89
Martin Valgur <martin.valgur@gmail.com>
2025-01-13 21:51:39 +0200

    catch2: remove leftover apply_conandata_patches()

--- recipes/catch2/3.x.x/conanfile.py
+++ recipes/catch2/3.x.x/conanfile.py
@@ -76,7 +76,6 @@ class Catch2Conan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
