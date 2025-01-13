4de134df4c08fd654327abd86c32e7b40cd33326
Martin Valgur <martin.valgur@gmail.com>
2025-01-13 22:24:36 +0200

    range-v3: add missing cmake_target_name properties

--- recipes/range-v3/all/conanfile.py
+++ recipes/range-v3/all/conanfile.py
@@ -45,33 +45,33 @@ class Rangev3Conan(ConanFile):
 
     def validate(self):
         check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(
-            str(self.settings.compiler), False)
-        if not minimum_version:
-            self.output.warning(
-                f"{self.settings.compiler} {self.settings.compiler.version} support for range-v3 is unknown, assuming it is supported.")
-        elif Version(self.settings.compiler.version) < minimum_version:
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(
-                f"range-v3 {self.version} requires C++{self._min_cppstd} with {self.settings.compiler}, which is not supported by {self.settings.compiler} {self.settings.compiler.version}")
+                f"range-v3 {self.version} requires C++{self._min_cppstd} with {self.settings.compiler},"
+                f" which is not supported by {self.settings.compiler} {self.settings.compiler.version}"
+            )
 
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        copy(self, "*", dst=os.path.join(self.package_folder, "include"),
-             src=os.path.join(self.source_folder, "include"))
-        copy(self, "LICENSE.txt", dst=os.path.join(
-            self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, "*", os.path.join(self.source_folder, "include"), os.path.join(self.package_folder, "include"))
+        copy(self, "LICENSE.txt", self.source_folder, os.path.join(self.package_folder, "licenses"))
 
     def package_info(self):
         self.cpp_info.libdirs = []
         self.cpp_info.bindirs = []
+
+        self.cpp_info.set_property("cmake_file_name", "range-v3")
+        self.cpp_info.set_property("cmake_target_name", "range-v3::range-v3")
+
+        self.cpp_info.components["range-v3-meta"].set_property("cmake_target_name", "range-v3::meta")
         if is_msvc(self):
             self.cpp_info.components["range-v3-meta"].cxxflags = ["/permissive-"]
-
             if "0.9.0" <= Version(self.version) < "0.11.0":
-                self.cpp_info.components["range-v3-meta"].cxxflags.append(
-                    "/experimental:preprocessor")
+                self.cpp_info.components["range-v3-meta"].cxxflags.append("/experimental:preprocessor")
+
+        self.cpp_info.components["range-v3-concepts"].set_property("cmake_target_name", "range-v3::concepts")
         self.cpp_info.components["range-v3-concepts"].requires = ["range-v3-meta"]
