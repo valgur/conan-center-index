e16f693054a2824b29d62d1f158974c57c0cd2ea
Martin Valgur <martin.valgur@gmail.com>
2025-01-19 22:45:42 +0200

    proj: drop outdated patches

--- recipes/proj/all/conanfile.py
+++ recipes/proj/all/conanfile.py
@@ -4,8 +4,7 @@ from conan import ConanFile
 from conan.tools.apple import is_apple_os
 from conan.tools.build import stdcpp_library
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import export_conandata_patches, get, copy, rmdir, replace_in_file, collect_libs, rm, rename
-from conan.tools.microsoft import is_msvc
+from conan.tools.files import export_conandata_patches, get, copy, rmdir, replace_in_file, collect_libs, rename
 from conan.tools.scm import Version
 
 required_conan_version = ">=2.0.5"
@@ -43,9 +42,6 @@ class ProjConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-        if Version(self.version) < "7.0.0":
-            del self.options.with_tiff
-            del self.options.with_curl
 
     def configure(self):
         if self.options.shared:
@@ -79,21 +75,12 @@ class ProjConan(ConanFile):
         tc.variables["BUILD_GIE"] = self.options.build_executables
         tc.variables["BUILD_PROJ"] = self.options.build_executables
         tc.variables["BUILD_PROJINFO"] = self.options.build_executables
-        if Version(self.version) < "9.1.0":
-            tc.variables["PROJ_DATA_SUBDIR"] = "res"
-        if Version(self.version) < "7.0.0":
-            tc.variables["PROJ_TESTS"] = False
-            tc.variables["BUILD_LIBPROJ_SHARED"] = self.options.shared
-            tc.variables["ENABLE_LTO"] = False
-            tc.variables["JNI_SUPPORT"] = False
-        else:
-            tc.variables["ENABLE_TIFF"] = self.options.with_tiff
-            tc.variables["ENABLE_CURL"] = self.options.with_curl
-            tc.variables["BUILD_TESTING"] = False
-            tc.variables["ENABLE_IPO"] = False
-            tc.variables["BUILD_PROJSYNC"] = self.options.build_executables and self.options.with_curl
-        if Version(self.version) >= "8.1.0":
-            tc.variables["NLOHMANN_JSON_ORIGIN"] = "external"
+        tc.variables["ENABLE_TIFF"] = self.options.with_tiff
+        tc.variables["ENABLE_CURL"] = self.options.with_curl
+        tc.variables["BUILD_TESTING"] = False
+        tc.variables["ENABLE_IPO"] = False
+        tc.variables["BUILD_PROJSYNC"] = self.options.build_executables and self.options.with_curl
+        tc.variables["NLOHMANN_JSON_ORIGIN"] = "external"
         tc.variables["CMAKE_MACOSX_BUNDLE"] = False
         if self.settings.os == "Linux":
             # Workaround for: https://github.com/conan-io/conan/issues/13560
@@ -110,32 +97,20 @@ class ProjConan(ConanFile):
 
         replace_in_file(self, cmakelists, "/W4", "")
 
-        # Let CMake install shared lib with a clean rpath !
-        if "7.1.0" <= Version(self.version) < "9.0.0":
-            replace_in_file(self, cmakelists, "set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)", "")
-
         # Aggressive workaround against SIP on macOS, to handle sqlite3 executable
         # linked to shared sqlite3 lib
         if is_apple_os(self):
-            if Version(self.version) < "8.1.0":
-                cmake_sqlite_call = "CMakeLists.txt"
-                pattern = "${EXE_SQLITE3}"
-            else:
-                cmake_sqlite_call = "generate_proj_db.cmake"
-                pattern = "\"${EXE_SQLITE3}\""
+            cmake_sqlite_call = "generate_proj_db.cmake"
+            pattern = '"${EXE_SQLITE3}"'
             lib_paths = self.dependencies.build["sqlite3"].cpp_info.libdirs
             replace_in_file(self,
                 os.path.join(self.source_folder, "data", cmake_sqlite_call),
                 f"COMMAND {pattern}",
-                f"COMMAND ${{CMAKE_COMMAND}} -E env \"DYLD_LIBRARY_PATH={':'.join(lib_paths)}\" {pattern}"
+                f'COMMAND ${{CMAKE_COMMAND}} -E env "DYLD_LIBRARY_PATH={":".join(lib_paths)}" {pattern}'
             )
 
-        # unvendor nlohmann_json
-        if Version(self.version) < "8.1.0":
-            rmdir(self, os.path.join(self.source_folder, "include", "proj", "internal", "nlohmann"))
-
         # Remove warning flags that are unfamiliar to GCC 5
-        if Version(self.version) >= "9.0" and self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "8.0":
+        if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "8.0":
             replace_in_file(self, os.path.join(self.source_folder, "src", "CMakeLists.txt"), "${PROJ_C_WARN_FLAGS}", "")
             replace_in_file(self, os.path.join(self.source_folder, "src", "CMakeLists.txt"), "${PROJ_CXX_WARN_FLAGS}", "")
 
@@ -151,21 +126,17 @@ class ProjConan(ConanFile):
         cmake.install()
         # recover the data ... 9.1.0 saves into share/proj rather than res directly
         # the new PROJ_DATA_PATH can't seem to be controlled from conan.
-        if Version(self.version) >= "9.1.0":
-            rename(self, src=os.path.join(self.package_folder, "share", "proj"), dst=os.path.join(self.package_folder, "res"))
+        rename(self, src=os.path.join(self.package_folder, "share", "proj"), dst=os.path.join(self.package_folder, "res"))
         # delete the rest of the deployed data
         rmdir(self, os.path.join(self.package_folder, "share"))
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
-        proj_version = Version(self.version)
-        cmake_config_filename = "proj" if proj_version >= "7.0.0" else "proj4"
-        cmake_namespace = "PROJ" if proj_version >= "7.0.0" else "PROJ4"
-        self.cpp_info.set_property("cmake_file_name", cmake_config_filename)
-        self.cpp_info.set_property("cmake_target_name", f"{cmake_namespace}::proj")
+        self.cpp_info.set_property("cmake_file_name", "proj")
+        self.cpp_info.set_property("cmake_target_name", "PROJ::proj")
         self.cpp_info.set_property("pkg_config_name", "proj")
-        self.cpp_info.components["projlib"].set_property("cmake_target_name", f"{cmake_namespace}::proj")
+        self.cpp_info.components["projlib"].set_property("cmake_target_name", "PROJ::proj")
         self.cpp_info.components["projlib"].set_property("pkg_config_name", "proj")
 
         self.cpp_info.components["projlib"].libs = collect_libs(self)
@@ -174,10 +145,7 @@ class ProjConan(ConanFile):
             if self.options.threadsafe:
                 self.cpp_info.components["projlib"].system_libs.append("pthread")
         elif self.settings.os == "Windows":
-            if proj_version >= "7.0.0":
-                self.cpp_info.components["projlib"].system_libs.append("shell32")
-            if proj_version >= "7.1.0":
-                self.cpp_info.components["projlib"].system_libs.append("ole32")
+            self.cpp_info.components["projlib"].system_libs.extend(["shell32", "ole32"])
         if not self.options.shared:
             libcxx = stdcpp_library(self)
             if libcxx:
@@ -187,15 +155,11 @@ class ProjConan(ConanFile):
             self.cpp_info.components["projlib"].requires.append("libtiff::tiff")
         if self.options.get_safe("with_curl"):
             self.cpp_info.components["projlib"].requires.append("libcurl::libcurl")
-        if Version(self.version) < "8.2.0":
-            if self.options.shared and is_msvc(self):
-                self.cpp_info.components["projlib"].defines.append("PROJ_MSVC_DLL_IMPORT")
-        else:
-            if not self.options.shared:
-                self.cpp_info.components["projlib"].defines.append("PROJ_DLL=")
+        if not self.options.shared:
+            self.cpp_info.components["projlib"].defines.append("PROJ_DLL=")
 
         # see https://proj.org/usage/environmentvars.html#envvar-PROJ_DATA
-        proj_data_env_var_name = "PROJ_LIB" if Version(self.version) < "9.1.0" else "PROJ_DATA"
+        proj_data_env_var_name = "PROJ_DATA"
         res_path = os.path.join(self.package_folder, "res")
         self.runenv_info.prepend_path(proj_data_env_var_name, res_path)
         if self.options.build_executables:
