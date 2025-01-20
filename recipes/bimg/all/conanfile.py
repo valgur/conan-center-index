e8ed5821a2204880b427984a5d5e6aea35ecd57b
Martin Valgur <martin.valgur@gmail.com>
2025-01-20 22:16:39 +0200

    bimg: use the correct compiler executable

--- recipes/bimg/all/conanfile.py
+++ recipes/bimg/all/conanfile.py
@@ -101,6 +101,9 @@ class bimgConan(ConanFile):
             tc.generate()
         else:
             tc = AutotoolsToolchain(self)
+            tc_env = tc.environment().vars(self)
+            for var, value in tc_env.items():
+                tc.make_args.append(f"{var}={value}")
             tc.generate()
 
     def build(self):
