7f0a8d8a3ce0c3bc0c5dca55af2b1725bdd22833
Martin Valgur <martin.valgur@gmail.com>
2025-01-21 22:08:42 +0200

    cppfront: fix test_package when cross-compiling

--- recipes/cppfront/all/test_package/conanfile.py
+++ recipes/cppfront/all/test_package/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.tools.build import can_run
 from conan.tools.files import copy
 from conan.tools.layout import basic_layout
 import os
@@ -6,7 +7,6 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "VirtualRunEnv"
 
     def layout(self):
         basic_layout(self)
@@ -15,9 +15,11 @@ class TestPackageConan(ConanFile):
         self.requires(self.tested_reference_str)
 
     def build(self):
-        copy(self, "pure2-hello.cpp2", src=self.recipe_folder, dst=self.build_folder)
-        self.run("cppfront {}".format(os.path.join(self.build_folder, "pure2-hello.cpp2")), env="conanrun")
+        if can_run(self):
+            copy(self, "pure2-hello.cpp2", src=self.recipe_folder, dst=self.build_folder)
+            self.run("cppfront {}".format(os.path.join(self.build_folder, "pure2-hello.cpp2")), env="conanrun")
 
     def test(self):
-        self.run("cppfront -h", env="conanrun")
-        assert os.path.isfile(os.path.join(self.build_folder, "pure2-hello.cpp"))
+        if can_run(self):
+            self.run("cppfront -h", env="conanrun")
+            assert os.path.isfile(os.path.join(self.build_folder, "pure2-hello.cpp"))
