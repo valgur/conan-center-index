7753cf8905e6213f931c50c28686145fd12e0a61
Martin Valgur <martin.valgur@gmail.com>
2025-01-22 00:51:06 +0200

    flatcc: fix tc.variables being ignored due to CMP0077

--- recipes/flatcc/all/conanfile.py
+++ recipes/flatcc/all/conanfile.py
@@ -3,7 +3,6 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.build import cross_building, can_run
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get
 from conan.tools.microsoft import is_msvc
@@ -67,27 +66,26 @@ class FlatccConan(ConanFile):
                 raise ConanInvalidConfiguration("Building flatcc libraries shared is not supported")
             if Version(self.version) == "0.6.0" and self.settings.compiler == "gcc":
                 raise ConanInvalidConfiguration("Building flatcc with MinGW is not supported")
-        if cross_building(self) and not can_run(self):
-            raise ConanInvalidConfiguration(f"Cross-building for a non-native architecture ({self.settings.arch}) is not supported")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["FLATCC_PORTABLE"] = self.options.portable
-        tc.variables["FLATCC_GNU_POSIX_MEMALIGN"] = self.options.gnu_posix_memalign
-        tc.variables["FLATCC_RTONLY"] = self.options.runtime_lib_only
-        tc.variables["FLATCC_INSTALL"] = True
-        tc.variables["FLATCC_COVERAGE"] = False
-        tc.variables["FLATCC_DEBUG_VERIFY"] = self.options.verify_assert
-        tc.variables["FLATCC_TRACE_VERIFY"] = self.options.verify_trace
-        tc.variables["FLATCC_REFLECTION"] = self.options.reflection
-        tc.variables["FLATCC_NATIVE_OPTIM"] = self.options.native_optim
-        tc.variables["FLATCC_FAST_DOUBLE"] = self.options.fast_double
-        tc.variables["FLATCC_IGNORE_CONST_COND"] = self.options.ignore_const_condition
-        tc.variables["FLATCC_TEST"] = False
-        tc.variables["FLATCC_ALLOW_WERROR"] = False
+        tc.cache_variables["FLATCC_PORTABLE"] = self.options.portable
+        tc.cache_variables["FLATCC_GNU_POSIX_MEMALIGN"] = self.options.gnu_posix_memalign
+        tc.cache_variables["FLATCC_RTONLY"] = self.options.runtime_lib_only
+        tc.cache_variables["FLATCC_INSTALL"] = True
+        tc.cache_variables["FLATCC_COVERAGE"] = False
+        tc.cache_variables["FLATCC_DEBUG_VERIFY"] = self.options.verify_assert
+        tc.cache_variables["FLATCC_TRACE_VERIFY"] = self.options.verify_trace
+        tc.cache_variables["FLATCC_REFLECTION"] = self.options.reflection
+        tc.cache_variables["FLATCC_NATIVE_OPTIM"] = self.options.native_optim
+        tc.cache_variables["FLATCC_FAST_DOUBLE"] = self.options.fast_double
+        tc.cache_variables["FLATCC_IGNORE_CONST_COND"] = self.options.ignore_const_condition
+        tc.cache_variables["FLATCC_TEST"] = False
+        tc.cache_variables["FLATCC_ALLOW_WERROR"] = False
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
         tc.generate()
 
     def build(self):
