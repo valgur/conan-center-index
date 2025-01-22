8b76ddc150b2f816e9bc69c44cbe5e719db4f5b6
Martin Valgur <martin.valgur@gmail.com>
2025-01-22 16:25:13 +0200

    matplotlib-cpp: requires cpython in test_package

--- recipes/matplotlib-cpp/all/test_package/conanfile.py
+++ recipes/matplotlib-cpp/all/test_package/conanfile.py
@@ -11,6 +11,9 @@ class TestPackageConan(ConanFile):
     def requirements(self):
         self.requires(self.tested_reference_str)
 
+    def build_requirements(self):
+        self.tool_requires("cpython/[~3.12]")
+
     def layout(self):
         cmake_layout(self)
 
