9138a3e423a7375f5645ca1b736729cbaf2decde
Martin Valgur <martin.valgur@gmail.com>
2025-01-31 17:28:48 +0200

    ffmpeg: pkgconf is required on Linux as well
    For libaom, for example.
    Also default to the correct executable when using pkgconf from Conan.

--- recipes/ffmpeg/all/conanfile.py
+++ recipes/ffmpeg/all/conanfile.py
@@ -371,8 +371,7 @@ class FFMpegConan(ConanFile):
                 self.tool_requires("nasm/2.16.01")
             else:
                 self.tool_requires("yasm/1.3.0")
-        if self.settings.os != "Linux" and not self.conf.get("tools.gnu:pkg_config", check_type=str):
-            # See https://github.com/conan-io/conan-center-index/pull/26447#discussion_r1926682155
+        if not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/[>=2.1 <3]")
         if self.settings_build.os == "Windows":
             self.win_bash = True
@@ -646,7 +645,7 @@ class FFMpegConan(ConanFile):
         if cross_building(self):
             build_cc = AutotoolsToolchain(self).vars()["CC_FOR_BUILD"]
             args.append(f"--host-cc={unix_path(self, build_cc)}")
-        pkg_config = self.conf.get("tools.gnu:pkg_config", default=buildenv_vars.get("PKG_CONFIG"), check_type=str)
+        pkg_config = self.conf.get("tools.gnu:pkg_config", default=buildenv_vars.get("PKG_CONFIG", "pkgconf"), check_type=str)
         if pkg_config:
             # the ffmpeg configure script hardcodes the name of the executable,
             # unlike other tools that use the PKG_CONFIG environment variable
@@ -670,7 +669,8 @@ class FFMpegConan(ConanFile):
         if tc.ldflags:
             args.append("--extra-ldflags={}".format(" ".join(tc.ldflags)))
         tc.configure_args.extend(args)
-        tc.generate()
+        env = tc.environment()
+        tc.generate(env)
 
         if is_msvc(self):
             # Custom AutotoolsDeps for cl like compilers
