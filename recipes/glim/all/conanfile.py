7d2a742257b68c2fe8719da76d0c580e9958c753
Martin Valgur <martin.valgur@gmail.com>
2025-01-31 15:00:36 +0200

    glim: fix gtsam_points version

--- recipes/glim/all/conanfile.py
+++ recipes/glim/all/conanfile.py
@@ -60,7 +60,7 @@ class GlimPackage(ConanFile):
         self.requires("eigen/3.4.0", transitive_headers=True, transitive_libs=True)
         self.requires("opencv/4.9.0", transitive_headers=True, transitive_libs=True)
         self.requires("gtsam/4.2", transitive_headers=True, transitive_libs=True)
-        self.requires("gtsam_points/1.0.4", transitive_headers=True, transitive_libs=True, options={"cuda": self.options.cuda})
+        self.requires("gtsam_points/1.0.6", transitive_headers=True, transitive_libs=True, options={"cuda": self.options.cuda})
         self.requires("nlohmann_json/3.11.3", transitive_headers=True, transitive_libs=True)
         self.requires("openmp/system")
         self.requires("spdlog/1.14.1", transitive_headers=True, transitive_libs=True)
