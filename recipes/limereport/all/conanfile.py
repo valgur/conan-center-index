508ae790381222fac5e8f53066ad0d3d20585f00
Martin Valgur <martin.valgur@gmail.com>
2025-02-02 21:35:45 +0200

    limereport: always use tool_requires for Qt tools

--- recipes/limereport/all/conanfile.py
+++ recipes/limereport/all/conanfile.py
@@ -2,7 +2,7 @@ import os
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd, can_run
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.files import copy, get, replace_in_file
 from conan.tools.microsoft import is_msvc
@@ -27,32 +27,28 @@ class LimereportConan(ConanFile):
         "fPIC": True,
         "with_zint": False,
     }
+    implements = ["auto_shared_fpic"]
 
     @property
     def _qt_version_major(self):
         return self.dependencies["qt"].ref.version.major
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-        self.options["qt"].qtdeclarative = True
-        self.options["qt"].qtsvg = True
-        self.options["qt"].qttools = True
-        if self.options.with_zint:
-            self.options["zint"].with_qt = True
-
     def layout(self):
         cmake_layout(self, src_folder="src")
 
+    @property
+    def _qt_options(self):
+        return {
+            "qtdeclarative": True,
+            "qtsvg": True,
+            "qttools": True,
+        }
+
     def requirements(self):
         # QString included in Irglobal.h and Limereport expects be running Qt on customer side
-        self.requires("qt/[>=5.15 <7]", transitive_headers=True, transitive_libs=True, run=can_run(self))
+        self.requires("qt/[>=5.15 <7]", transitive_headers=True, transitive_libs=True, options=self._qt_options)
         if self.options.with_zint:
-            self.requires("zint/2.10.0")
+            self.requires("zint/2.10.0", options={"with_qt": True})
 
     def validate(self):
         check_min_cppstd(self, 17)
@@ -64,13 +60,18 @@ class LimereportConan(ConanFile):
             raise ConanInvalidConfiguration(f"{self.ref} option with_zint=True requires -o 'zint/*:with_qt=True'")
 
     def build_requirements(self):
-        if not can_run(self):
-            self.tool_requires("qt/<host_version>", options={"gui": False, "widgets": False})
+        self.tool_requires("cmake/[>=3.21 <4]")
+        self.tool_requires("qt/<host_version>", options=self._qt_options)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
         # Avoid using vendorized zint
         replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"), "add_subdirectory(3rdparty)", "")
+
+    def _qt_tool(self, tool):
+        tools_dir = self.dependencies.build["qt"].conf_info.get("user.qt:tools_directory")
+        suffix = ".exe" if self.settings_build.os == "Windows" else ""
+        return os.path.join(tools_dir, tool + suffix).replace("\\", "/")
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -79,6 +80,9 @@ class LimereportConan(ConanFile):
             tc.cache_variables["WINDOWS_BUILD"] = True
         tc.cache_variables["USE_QT6"] = self._qt_version_major == 6
         tc.cache_variables["ENABLE_ZINT"] = self.options.with_zint
+        tc.cache_variables["CMAKE_AUTOMOC_EXECUTABLE"] = self._qt_tool("moc")
+        tc.cache_variables["CMAKE_AUTOUIC_EXECUTABLE"] = self._qt_tool("uic")
+        tc.cache_variables["CMAKE_AUTORCC_EXECUTABLE"] = self._qt_tool("rcc")
         tc.generate()
         tc = CMakeDeps(self)
         tc.generate()
