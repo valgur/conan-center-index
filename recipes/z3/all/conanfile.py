ce1264fe247b643b9f058738af1e71ac1fa2f3bb
Martin Valgur <martin.valgur@gmail.com>
2025-02-06 23:00:53 +0200

    z3: remove unused imports

--- recipes/z3/all/conanfile.py
+++ recipes/z3/all/conanfile.py
@@ -1,12 +1,10 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import get, copy, rmdir
-from conan.tools.scm import Version
 
 required_conan_version = ">=2"
 
