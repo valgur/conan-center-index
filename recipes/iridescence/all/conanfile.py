4ae11793cecc2acfd51ae62200cccda2d5cb42d0
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 18:08:48 +0200

    iridescence: drop Conan v1 support

--- recipes/iridescence/all/conanfile.py
+++ recipes/iridescence/all/conanfile.py
@@ -32,19 +32,6 @@ class IridescenceConan(ConanFile):
     }
     implements = ["auto_shared_fpic"]
 
-    @property
-    def _min_cppstd(self):
-        return 17
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "gcc": "9",
-            "clang": "9",
-            "apple-clang": "11",
-            "msvc": "192",
-        }
-
     def export_sources(self):
         export_conandata_patches(self)
 
@@ -69,12 +56,7 @@ class IridescenceConan(ConanFile):
         self.requires("khrplatform/cci.20200529", transitive_headers=True)
 
     def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 17)
 
         if is_msvc(self):
             # Build fails with several compilation errors
