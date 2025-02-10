c6908b9c16d43517c411c35f943622d3f2f72e0a
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 17:44:15 +0200

    hscpp: drop Conan v1 support

--- recipes/hscpp/all/conanfile.py
+++ recipes/hscpp/all/conanfile.py
@@ -8,7 +8,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import *
 from conan.tools.microsoft import is_msvc
 
-required_conan_version = ">=2.0"
+required_conan_version = ">=2.1"
 
 
 class HscppConan(ConanFile):
@@ -52,6 +52,9 @@ class HscppConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
         rmdir(self, "lib")
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                        "add_subdirectory(lib/filesystem-1.3.4)",
+                        "find_package(ghc_filesystem REQUIRED CONFIG)")
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -67,13 +70,7 @@ class HscppConan(ConanFile):
         deps.set_property("ghc-filesystem", "cmake_target_name", "ghc_filesystem")
         deps.generate()
 
-    def _patch_sources(self):
-        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
-                        "add_subdirectory(lib/filesystem-1.3.4)",
-                        "find_package(ghc_filesystem REQUIRED CONFIG)")
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure(build_script_folder=Path(self.source_folder).parent)
         cmake.build()
