d3ce2a06fed1546994b5380442764fbd04ac9b84
Martin Valgur <martin.valgur@gmail.com>
2025-03-26 15:12:59 +0200

    nanopb: add a new recipe
    Resolves https://github.com/conan-io/conan/issues/18024

---
+++ recipes/nanopb/all/conanfile.py
@@ -0,0 +1,50 @@
+import os
+
+from conan import ConanFile
+from conan.tools.files import download, rm
+
+required_conan_version = ">=2.0"
+
+
+class NanopbConan(ConanFile):
+    name = "nanopb"
+    description = ("Nanopb is a small code-size Protocol Buffers implementation in ansi C. "
+                   "It is especially suitable for use in microcontrollers, but fits any memory restricted system.")
+    license = "Zlib"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://jpa.kapsi.fi/nanopb/"
+    topics = ("protocol-buffers", "protobuf", "microcontrollers")
+    package_type = "application"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def package_id(self):
+        self.info.clear()
+
+    def build_requirements(self):
+        self.tool_requires("cpython/[*]", visible=True)
+
+    @property
+    def _site_packages_dir(self):
+        python_ver = self.dependencies.build["cpython"].ref.version
+        return os.path.join(self.package_folder, "lib", f"python{python_ver.major}.{python_ver.minor}", "site-packages")
+
+    def package(self):
+        download(self, **self.conan_data["sources"][self.version]["license"],
+                 filename=os.path.join(self.package_folder, "licenses", "LICENSE.txt"))
+        pkgs = " ".join([
+            f"nanopb=={self.version}",
+            # protobuf v6 is too new for nanopb v0.4.9.1
+            f"protobuf==5.*",
+        ])
+        self.run(f'python -m pip install {pkgs} --no-cache-dir --target="{self._site_packages_dir}"')
+        rm(self, "*.pyc", self.package_folder, recursive=True)
+
+    def package_info(self):
+        self.cpp_info.frameworkdirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
+        self.cpp_info.includedirs = []
+
+        self.buildenv_info.prepend_path("PYTHONPATH", self._site_packages_dir)
+        self.runenv_info.prepend_path("PYTHONPATH", self._site_packages_dir)
+        self.cpp_info.bindirs = [os.path.join(self._site_packages_dir, "bin")]
---
+++ recipes/nanopb/all/test_package/conanfile.py
@@ -0,0 +1,17 @@
+import os
+
+from conan import ConanFile
+from conan.tools.layout import basic_layout
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+
+    def layout(self):
+        basic_layout(self)
+
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
+
+    def test(self):
+        self.run(f"nanopb_generator --proto-path {self.source_folder} simple.proto")
