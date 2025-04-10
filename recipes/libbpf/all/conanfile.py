0c042b8437c49c6ddad9d7ca9ec98b4a198e43fd
Martin Valgur <martin.valgur@gmail.com>
2025-04-10 16:02:47 +0300

    libbpf: simplify

--- recipes/libbpf/all/conanfile.py
+++ recipes/libbpf/all/conanfile.py
@@ -2,7 +2,6 @@ import os
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import *
 from conan.tools.gnu import Autotools, PkgConfigDeps, GnuToolchain
 from conan.tools.layout import basic_layout
@@ -61,8 +60,6 @@ class LibbpfConan(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
-        VirtualBuildEnv(self).generate()
-
         tc = GnuToolchain(self)
         tc_vars = tc.extra_env.vars(self)
         tc.make_args["PREFIX"] = "/"
