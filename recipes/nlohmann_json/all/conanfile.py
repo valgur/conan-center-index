8e7f0cff7e2f91a4d8cb8d3343e33c1360465f5c
Martin Valgur <martin.valgur@gmail.com>
2025-04-15 16:52:21 +0300

    nlohmann_json: add v3.12.0, drop old versions

--- recipes/nlohmann_json/all/conanfile.py
+++ recipes/nlohmann_json/all/conanfile.py
@@ -1,8 +1,9 @@
+import os
+
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import *
 from conan.tools.layout import basic_layout
-import os
 
 required_conan_version = ">=2.1"
 
@@ -18,10 +19,6 @@ class NlohmannJsonConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    @property
-    def _minimum_cpp_standard(self):
-        return 11
-
     def layout(self):
         basic_layout(self, src_folder="src")
 
@@ -29,16 +26,10 @@ class NlohmannJsonConan(ConanFile):
         self.info.clear()
 
     def validate(self):
-        check_min_cppstd(self, self._minimum_cpp_standard)
+        check_min_cppstd(self, 11)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-
-    def generate(self):
-        pass
-
-    def build(self):
-        pass
 
     def package(self):
         copy(self, "LICENSE*", self.source_folder, os.path.join(self.package_folder, "licenses"))
