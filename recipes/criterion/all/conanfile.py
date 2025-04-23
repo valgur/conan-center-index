7f56c1b22ff40ac76cf4b55f527809de489294ee
Martin Valgur <martin.valgur@gmail.com>
2025-04-24 00:09:58 +0300

    criterion: add recipe
    Resolves https://github.com/conan-io/conan-center-index/pull/16711

---
+++ recipes/criterion/all/conanfile.py
@@ -0,0 +1,94 @@
+import os
+
+from conan import ConanFile
+from conan.tools.apple import fix_apple_shared_install_name
+from conan.tools.build import check_min_cppstd, check_min_cstd
+from conan.tools.files import *
+from conan.tools.gnu import PkgConfigDeps
+from conan.tools.layout import basic_layout
+from conan.tools.meson import Meson, MesonToolchain
+
+required_conan_version = ">=2.0"
+
+
+class CriterionConan(ConanFile):
+    name = "criterion"
+    description = "C and C++ unit testing framework for the 21st century"
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/Snaipe/Criterion"
+    topics = ("unit-testing", "testing")
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+    implements = ["auto_shared_fpic"]
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("boxfort/[^0.1.4]")
+        self.requires("debugbreak/[^1.0]")
+        self.requires("klib/[*]")
+        self.requires("libffi/[^3.4.6]")
+        self.requires("libgit2/[^1.8.4]")
+        self.requires("nanomsg/[^1.2.1]")
+        self.requires("nanopb/[^0.4.9.1]")
+
+    def validate_build(self):
+        check_min_cppstd(self, 11)
+        if self.settings.compiler.get_safe("cstd"):
+            check_min_cstd(self, 11)
+
+    def build_requirements(self):
+        self.tool_requires("meson/[>=1.2.3 <2]")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/[>=2.2 <3]")
+        self.tool_requires("nanopb/<host_version>")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        replace_in_file(self, "meson.build", ", method: 'cmake'", "")
+
+    def generate(self):
+        tc = MesonToolchain(self)
+        tc.generate()
+        deps = PkgConfigDeps(self)
+        deps.generate()
+
+    def build(self):
+        meson = Meson(self)
+        meson.configure()
+        meson.build()
+
+    def package(self):
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        meson = Meson(self)
+        meson.install()
+        if self.options.shared:
+            rm(self, "*.a", os.path.join(self.package_folder, "lib"))
+            rm(self, "*.lib", os.path.join(self.package_folder, "lib"))
+        else:
+            rm(self, "*.dll", os.path.join(self.package_folder, "bin"))
+            rm(self, "*.so*", os.path.join(self.package_folder, "lib"))
+            rm(self, "*.dylib", os.path.join(self.package_folder, "lib"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rm(self, "*.pdb", self.package_folder, recursive=True)
+        fix_apple_shared_install_name(self)
+
+    def package_info(self):
+        self.cpp_info.set_property("pkg_config_name", "criterion")
+        self.cpp_info.libs = ["criterion"]
+        self.cpp_info.resdirs = ["share"]
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.extend(["pthread", "rt", "m"])
