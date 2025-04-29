ad1cf06cd24ab7a6ad0c5712b729b3659372b109
Martin Valgur <martin.valgur@gmail.com>
2025-04-29 22:26:53 +0300

    litehtml: add v0.9

--- recipes/litehtml/all/conanfile.py
+++ recipes/litehtml/all/conanfile.py
@@ -1,11 +1,12 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
+from conan.errors import ConanInvalidConfiguration, ConanException
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import *
 from conan.tools.microsoft import is_msvc
+from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
@@ -23,40 +24,21 @@ class LitehtmlConan(ConanFile):
         "shared": [True, False],
         "fPIC": [True, False],
         "utf8": [True, False],
-        "with_icu": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
         "utf8": False,
-        "with_icu": False,
     }
+    implements = ["auto_shared_fpic"]
 
     @property
     def _with_xxd(self):
         # FIXME: create conan recipe for xxd, and use it unconditionally (returning False means cross build doesn't work)
         return self.settings.os != "Windows"
 
-    def export_sources(self):
-        export_conandata_patches(self)
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-        if self.version != "cci.20211028":
-            del self.options.with_icu
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-
     def layout(self):
         cmake_layout(self, src_folder="src")
-
-    def requirements(self):
-        # FIXME: add gumbo requirement (it is vendored right now)
-        if self.options.get_safe("with_icu"):
-            self.requires("icu/[*]")
 
     def validate(self):
         check_min_cppstd(self, 11)
@@ -69,15 +51,19 @@ class LitehtmlConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["BUILD_TESTING"] = False
+        if Version(self.version) >= "0.9":
+            tc.variables["LITEHTML_BUILD_TESTING"] = False
+        else:
+            tc.variables["BUILD_TESTING"] = False
         tc.variables["LITEHTML_UTF8"] = self.options.utf8
-        tc.variables["USE_ICU"] = self.options.get_safe("with_icu", False)
         tc.variables["EXTERNAL_GUMBO"] = False # FIXME: add cci recipe, and use it unconditionally (option value should be True)
         tc.variables["EXTERNAL_XXD"] = self._with_xxd  # FIXME: should be True unconditionally
+        tc.cache_variables["CMAKE_POLICY_VERSION_MINIMUM"] = "3.15" # CMake 4 support
+        if Version(self.version) > "0.9":
+            raise ConanException("CMAKE_POLICY_VERSION_MINIMUM hardcoded to 3.5, check if new version supports CMake 4")
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
@@ -100,8 +86,6 @@ class LitehtmlConan(ConanFile):
         self.cpp_info.components["litehtml_litehtml"].set_property("cmake_target_name", "litehtml")
         self.cpp_info.components["litehtml_litehtml"].libs = ["litehtml"]
         self.cpp_info.components["litehtml_litehtml"].requires = ["gumbo"]
-        if self.options.get_safe("with_icu"):
-            self.cpp_info.components["litehtml_litehtml"].requires.append("icu::icu")
 
         if True: # FIXME: remove once we use a vendored gumbo library
             self.cpp_info.components["gumbo"].set_property("cmake_target_name", "gumbo")
