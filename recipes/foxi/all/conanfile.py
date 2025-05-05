0a7c2ea4d7cd8c36a4e45e037f7f4341672f8936
Martin Valgur <martin.valgur@gmail.com>
2025-05-05 23:53:44 +0300

    foxi: add CMake v4 support

--- recipes/foxi/all/conanfile.py
+++ recipes/foxi/all/conanfile.py
@@ -5,7 +5,7 @@ from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import *
 
-required_conan_version = ">=2.1"
+required_conan_version = ">=2.4"
 
 
 class FoxiConan(ConanFile):
@@ -23,24 +23,11 @@ class FoxiConan(ConanFile):
     default_options = {
         "fPIC": True,
     }
+    implements = ["auto_shared_fpic"]
+    languages = ["C"]
 
     def export_sources(self):
-        for p in self.conan_data.get("patches", {}).get(self.version, []):
-            copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        try:
-            del self.settings.compiler.libcxx
-        except Exception:
-            pass
-        try:
-            del self.settings.compiler.cppstd
-        except Exception:
-            pass
+        export_conandata_patches(self)
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -48,22 +35,19 @@ class FoxiConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
         apply_conandata_patches(self)
+        replace_in_file(self, "CMakeLists.txt",
+                        "cmake_minimum_required(VERSION 3.1)",
+                        "cmake_minimum_required(VERSION 3.15)")
+        replace_in_file(self, "CMakeLists.txt", "add_msvc_runtime_flag(foxi_loader)", "")
+        replace_in_file(self, "CMakeLists.txt", "add_msvc_runtime_flag(foxi_dummy)", "")
+        replace_in_file(self, "CMakeLists.txt", "DESTINATION lib", "RUNTIME DESTINATION bin ARCHIVE DESTINATION lib LIBRARY DESTINATION lib")
 
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["FOXI_WERROR"] = False
         tc.generate()
 
-    def _patch_sources(self):
-        cmakelists = os.path.join(self.source_folder, "CMakeLists.txt")
-        replace_in_file(self, cmakelists, "add_msvc_runtime_flag(foxi_loader)", "")
-        replace_in_file(self, cmakelists, "add_msvc_runtime_flag(foxi_dummy)", "")
-        replace_in_file(self, cmakelists,
-                              "DESTINATION lib",
-                              "RUNTIME DESTINATION bin ARCHIVE DESTINATION lib LIBRARY DESTINATION lib")
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
