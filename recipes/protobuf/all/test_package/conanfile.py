e0270bfe8d3cc204763fe5edf2b6c34a6047dcae
Martin Valgur <martin.valgur@gmail.com>
2025-05-05 12:55:40 +0300

    protobuf: more accurate test_package, clean up

--- recipes/protobuf/all/test_package/conanfile.py
+++ recipes/protobuf/all/test_package/conanfile.py
@@ -2,27 +2,21 @@ import os
 
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
-from conan.tools.scm import Version
+from conan.tools.cmake import cmake_layout, CMake
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps"
+    generators = "CMakeToolchain", "CMakeDeps"
 
     def layout(self):
         cmake_layout(self)
 
     def requirements(self):
-        # note `run=True` so that the runenv can find protoc
-        self.requires(self.tested_reference_str, run=True)
+        self.requires(self.tested_reference_str)
 
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.cache_variables["protobuf_LITE"] = self.dependencies[self.tested_reference_str].options.lite
-        protobuf_version = Version(self.dependencies[self.tested_reference_str].ref.version)
-        tc.cache_variables["CONAN_TEST_USE_CXXSTD_14"] = protobuf_version >= "3.22"
-        tc.generate()
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
@@ -34,7 +28,8 @@ class TestPackageConan(ConanFile):
             bin_path = os.path.join(self.cpp.build.bindir, "test_package")
             self.run(bin_path, env="conanrun")
 
-            # Invoke protoc in the same way CMake would
-            self.run(f"protoc --proto_path={self.source_folder} --cpp_out={self.build_folder} {self.source_folder}/addressbook.proto", env="conanrun")
-            assert os.path.exists(os.path.join(self.build_folder,"addressbook.pb.cc"))
-            assert os.path.exists(os.path.join(self.build_folder,"addressbook.pb.h"))
+        self.run("protoc --version")
+        # Invoke protoc in the same way CMake would
+        self.run(f"protoc --proto_path={self.source_folder} --cpp_out={self.build_folder} {self.source_folder}/addressbook.proto")
+        assert os.path.exists(os.path.join(self.build_folder,"addressbook.pb.cc"))
+        assert os.path.exists(os.path.join(self.build_folder,"addressbook.pb.h"))
