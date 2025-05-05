f214193dc363eecdc929261c2a67f3410d562561
Martin Valgur <martin.valgur@gmail.com>
2025-05-05 23:59:56 +0300

    libtorch: use a version range for protobuf

--- recipes/libtorch/all/conanfile.py
+++ recipes/libtorch/all/conanfile.py
@@ -247,7 +247,7 @@ class LibtorchConan(ConanFile):
         self.requires("fmt/[^10.2.1]", transitive_headers=True, transitive_libs=True)
         self.requires("foxi/cci.20210217", libs=False)
         self.requires("onnx/1.17.0", transitive_headers=True, transitive_libs=True)
-        self.requires("protobuf/3.21.12")
+        self.requires("protobuf/[>=3.21.12]")
         self.requires("fp16/[>=cci.20210320]")
         self.requires("cpp-httplib/0.18.0")
         self.requires("libbacktrace/cci.20240730")
