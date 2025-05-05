98b921cf02ed98983502a4e72d63510886814551
Martin Valgur <martin.valgur@gmail.com>
2025-05-05 20:34:12 +0300

    foxglove-schemas-protobuf: use a version range for protobuf

--- recipes/foxglove-schemas-protobuf/all/conanfile.py
+++ recipes/foxglove-schemas-protobuf/all/conanfile.py
@@ -5,8 +5,6 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 from conan.tools.files import *
-from conan.tools.microsoft import check_min_vs
-from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
@@ -31,40 +29,19 @@ class FoxgloveSchemasProtobufConan(ConanFile):
     }
     implements = ["auto_shared_fpic"]
 
-    @property
-    def _min_cppstd(self):
-        return 17
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "msvc": "191",
-            "gcc": "9",
-            "clang": "9",
-            "apple-clang": "12",
-        }
-
     def export_sources(self):
         copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=(self.export_sources_folder + "/src"))
-        export_conandata_patches(self)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        apply_conandata_patches(self)
 
     def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
-        check_min_vs(self, self._compilers_minimum_version["msvc"])
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 17)
         if self.settings.os == "Windows" and self.options.shared:
             raise ConanInvalidConfiguration("Windows shared builds are not supported yet.")
 
     def requirements(self):
-        self.requires("protobuf/3.21.12", transitive_headers=True, transitive_libs=True)
+        self.requires("protobuf/[>=3.21.12]", transitive_headers=True, transitive_libs=True)
 
     def build_requirements(self):
         self.tool_requires("protobuf/<host_version>")
