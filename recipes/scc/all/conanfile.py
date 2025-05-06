921ca250c59a760ee1c7a4764219af905b6ba6fa
Martin Valgur <martin.valgur@gmail.com>
2025-05-06 15:03:43 +0300

    scc: add CMake v4 support

--- recipes/scc/all/conanfile.py
+++ recipes/scc/all/conanfile.py
@@ -51,7 +51,7 @@ class SystemcComponentsConan(ConanFile):
         self.requires("fmt/[^10.2.1]", transitive_headers=True, transitive_libs=True)
         # https://github.com/Minres/SystemC-Components/blob/2023.06/src/common/util/lz4_streambuf.h#L13
         self.requires("lz4/[^1.9.4]", transitive_headers=True, transitive_libs=True)
-        self.requires("rapidjson/cci.20220822")
+        self.requires("rapidjson/[^1.1.0]")
         self.requires("spdlog/1.13.0")
         # https://github.com/Minres/SystemC-Components/blob/2023.06/src/sysc/tlm/scc/lwtr/tlm2_lwtr.h
         self.requires("systemc-cci/1.0.0", transitive_headers=True, transitive_libs=True)
