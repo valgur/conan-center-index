861fe5eb7f451f4e9614797f8fc2e68f2b2b9e3b
Martin Valgur <martin.valgur@gmail.com>
2025-05-07 00:28:54 +0300

    cyclonedds: must use with tool_requires

--- recipes/cyclonedds/all/cmake/CycloneDDS_idlc.cmake
+++ recipes/cyclonedds/all/cmake/CycloneDDS_idlc.cmake
@@ -1,18 +1,9 @@
 if(NOT TARGET CycloneDDS::idlc)
-    if(CMAKE_CROSSCOMPILING)
-        find_program(_idlc_executable
-            NAMES idlc
-            PATHS ENV PATH
-            NO_DEFAULT_PATH
-        )
-    else()
-        find_program(_idlc_executable
-            NAMES idlc
-            PATHS "${CMAKE_CURRENT_LIST_DIR}/../../../bin/"
-            NO_DEFAULT_PATH
-        )
-    endif()
-
+    find_program(_idlc_executable
+        NAMES idlc
+        PATHS ENV PATH
+        NO_DEFAULT_PATH
+    )
     if(_idlc_executable)
         get_filename_component(_idlc_executable "${_idlc_executable}" ABSOLUTE)
         add_executable(CycloneDDS::idlc IMPORTED)
