d2f1d9de6a55b4e75ebd99be302f523a4e3caaf2
Martin Valgur <martin.valgur@gmail.com>
2025-05-07 00:17:59 +0300

    flatbuffers: must use with tool_requires

--- recipes/flatbuffers/all/test_package/conanfile.py
+++ recipes/flatbuffers/all/test_package/conanfile.py
@@ -3,7 +3,6 @@ import os
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.env import VirtualRunEnv
 
 
 class TestPackageConan(ConanFile):
@@ -14,14 +13,12 @@ class TestPackageConan(ConanFile):
         cmake_layout(self)
 
     def requirements(self):
-        self.requires(self.tested_reference_str, run=can_run(self))
+        self.requires(self.tested_reference_str)
+
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
 
     def generate(self):
-        env = VirtualRunEnv(self)
-        env.generate()
-        if can_run(self):
-            env = VirtualRunEnv(self)
-            env.generate(scope="build")
         tc = CMakeToolchain(self)
         tc.variables["FLATBUFFERS_HEADER_ONLY"] = self.dependencies["flatbuffers"].options.header_only
         tc.generate()
