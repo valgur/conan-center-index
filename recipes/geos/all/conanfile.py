1ef650948fca518cbba4da74ee098f05b0cbc131
Martin Valgur <martin.valgur@gmail.com>
2025-05-06 14:47:54 +0300

    geos: drop old versions

--- recipes/geos/all/conanfile.py
+++ recipes/geos/all/conanfile.py
@@ -1,11 +1,9 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd, stdcpp_library
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import *
-from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
@@ -22,40 +20,17 @@ class GeosConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "inline": [True, False],
         "utils": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "inline": True,
         "utils": True,
     }
-
-    @property
-    def _min_cppstd(self):
-        return "14" if Version(self.version) >= "3.12.0" else "11"
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "14": {
-                "gcc": "6",
-                "clang": "5",
-                "apple-clang": "10",
-                "msvc": "191",
-            },
-        }.get(self._min_cppstd, {})
-
-    @property
-    def _has_inline_option(self):
-        return Version(self.version) < "3.11.0"
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-        if not self._has_inline_option:
-            del self.options.inline
 
     def configure(self):
         if self.options.shared:
@@ -65,42 +40,22 @@ class GeosConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 14)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        if Version(self.version) < "3.11.0":
-            # these 2 options are declared before project() in geos < 3.11.0
-            tc.cache_variables["BUILD_SHARED_LIBS"] = self.options.shared
-            tc.cache_variables["BUILD_BENCHMARKS"] = False
-        else:
-            tc.variables["BUILD_BENCHMARKS"] = False
-            tc.cache_variables["CMAKE_BUILD_TYPE"] = str(self.settings.build_type)
-        if self._has_inline_option:
-            tc.variables["DISABLE_GEOS_INLINE"] = not self.options.inline
+        tc.variables["BUILD_BENCHMARKS"] = False
+        tc.cache_variables["CMAKE_BUILD_TYPE"] = str(self.settings.build_type)
         tc.variables["BUILD_TESTING"] = False
         tc.variables["BUILD_DOCUMENTATION"] = False
         tc.variables["BUILD_ASTYLE"] = False
         tc.variables["BUILD_GEOSOP"] = self.options.utils
         tc.generate()
 
-    def _patch_sources(self):
-        # Avoid setting CMAKE_BUILD_TYPE default when multi-config generators are used.
-        # https://github.com/libgeos/geos/pull/945
-        if Version(self.version) <= "3.12.1":
-            replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
-                            "set(CMAKE_BUILD_TYPE ${DEFAULT_BUILD_TYPE})", "")
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -124,8 +79,6 @@ class GeosConan(ConanFile):
         # GEOS::geos_cxx_flags
         self.cpp_info.components["geos_cxx_flags"].set_property("cmake_target_name", "GEOS::geos_cxx_flags")
         self.cpp_info.components["geos_cxx_flags"].defines.append("USE_UNSTABLE_GEOS_CPP_API")
-        if self.options.get_safe("inline"):
-            self.cpp_info.components["geos_cxx_flags"].defines.append("GEOS_INLINE")
         if self.settings.os == "Windows":
             self.cpp_info.components["geos_cxx_flags"].defines.append("TTMATH_NOASM")
 
