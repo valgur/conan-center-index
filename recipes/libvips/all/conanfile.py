c1edfa6fcbd57746663a2689a0fd6effc2539077
Martin Valgur <martin.valgur@gmail.com>
2025-05-07 11:23:44 +0300

    use a version range for highway

--- recipes/libvips/all/conanfile.py
+++ recipes/libvips/all/conanfile.py
@@ -150,7 +150,7 @@ class LibvipsConan(ConanFile):
         if self.options.with_heif:
             self.requires("libheif/[^1.16.2]")
         if self.options.get_safe("with_highway"):
-            self.requires("highway/1.0.7")
+            self.requires("highway/[^1.0.7]")
         if self.options.with_jpeg == "libjpeg":
             self.requires("libjpeg/9e")
         elif self.options.with_jpeg == "libjpeg-turbo":
