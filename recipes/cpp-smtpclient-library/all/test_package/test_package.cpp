6c565b48683a1735004668080e3359336d5aec4f
Martin Valgur <martin.valgur@gmail.com>
2025-05-20 21:49:36 +0300

    cpp-smtpclient-library: rename to smtpclient, clean up

--- recipes/cpp-smtpclient-library/all/test_package/test_package.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdlib>
-#include "smtpclient/cpp/credential.hpp"
-
-
-int main(void) {
-    jed_utils::cpp::Credential creds("Test", "Test2");
-
-    return EXIT_SUCCESS;
-}
---
+++ recipes/smtpclient/all/test_package/test_package.cpp
@@ -0,0 +1,5 @@
+#include <smtpclient/cpp/credential.hpp>
+
+int main() {
+    jed_utils::cpp::Credential creds("Test", "Test2");
+}
