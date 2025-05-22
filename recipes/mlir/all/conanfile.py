e4af4bdb47046e09ada33d67df2f9d1dc161cff4
Martin Valgur <martin.valgur@gmail.com>
2025-05-22 20:56:50 +0300

    mlir: export aggregated object file targets

--- recipes/mlir/all/conanfile.py
+++ recipes/mlir/all/conanfile.py
@@ -34,7 +34,7 @@ class MLIRConan(ConanFile):
     default_options = {
         "shared": True,
         "fPIC": True,
-        "install_aggregate_objects": False,
+        "install_aggregate_objects": True,
         "tools": False,
     }
     implements = ["auto_shared_fpic"]
@@ -104,13 +104,13 @@ class MLIRConan(ConanFile):
                         raise ConanException(f"Unexpected dependency for {component}: {req}")
 
     @cached_property
-    def _mlir_build_info(self):
+    def _build_info(self):
         return {
             "components": components_from_dotfile(self._graphviz_file.read_text()),
         }
 
     def _write_build_info(self, path):
-        Path(path).write_text(json.dumps(self._mlir_build_info, indent=2))
+        Path(path).write_text(json.dumps(self._build_info, indent=2))
 
     def _read_build_info(self) -> dict:
         return json.loads(self._build_info_file.read_text())
@@ -134,7 +134,7 @@ class MLIRConan(ConanFile):
         """))
         cmake.configure(build_script_folder="mlir", cli_args=graphviz_args)
         self._write_build_info(self._build_info_file.name)
-        self._validate_components(self._mlir_build_info["components"])
+        self._validate_components(self._build_info["components"])
         cmake.build()
 
     @property
@@ -149,26 +149,84 @@ class MLIRConan(ConanFile):
     def _build_info_file(self):
         return self._package_path / self._cmake_module_path / "conan_mlir_build_info.json"
 
+    @cached_property
+    def _obj_dir(self):
+        return Path(self.package_folder) / "lib" / f"objects-{self.settings.build_type}"
+
+    @cached_property
+    def _obj_libs(self):
+        return {d.name.replace("obj.", ""): sorted(d.glob("*.o")) for d in self._obj_dir.iterdir() if d.is_dir()}
+
     def package(self):
         copy(self, "LICENSE.TXT", self._source_path, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
 
+        # Back up original cmake files for debugging purposes
+        package_folder = Path(self.package_folder)
+        cmake_dir = package_folder / self._cmake_module_path
+        copy(self, "*", cmake_dir, os.path.join(self.package_folder, "share", "conan", self.name, "cmake_original"))
+
         self._write_build_info(self._build_info_file)
 
-        package_folder = Path(self.package_folder)
-        cmake_folder = package_folder / self._cmake_module_path
-        rm(self, "MLIRConfigVersion.cmake", cmake_folder)
-        rm(self, "MLIRTargets*", cmake_folder)
-        config_vars_cmake = cmake_folder / "MLIRConfigVars.cmake"
-        rename(self, cmake_folder / "MLIRConfig.cmake", config_vars_cmake)
+        rm(self, "MLIRConfigVersion.cmake", cmake_dir)
+        rm(self, "MLIRTargets*", cmake_dir)
+        config_vars_cmake = cmake_dir / "MLIRConfigVars.cmake"
+        rename(self, cmake_dir / "MLIRConfig.cmake", config_vars_cmake)
 
         replace_in_file(self, config_vars_cmake, 'include("${MLIR_CMAKE_DIR}/MLIRTargets.cmake")', "")
         # AddMLIR.cmake breaks if tablegen executables are not absolute paths
         for exe in ["MLIR_TABLEGEN_EXE", "MLIR_PDLL_TABLEGEN_EXE", "MLIR_SRC_SHARDER_TABLEGEN_EXE"]:
             replace_in_file(self, config_vars_cmake, f"set({exe} ", f"find_program({exe} ")
 
+        self._write_export_executables_cmake(cmake_dir / "conan_add_executable_targets.cmake")
+        if self.options.install_aggregate_objects:
+            self._write_export_objects_cmake(cmake_dir / "conan_add_object_targets.cmake")
+
         rmdir(self, package_folder / "share" / "man")
+
+    def _write_export_executables_cmake(self, cmake_file_path):
+        # MLIR export tools as CMake targets. Add a helper .cmake file to reproduce this in Conan.
+        bin_dir = Path(self.package_folder, "bin")
+        exe_pattern = "*.exe" if self.settings.os == "Windows" else "*"
+        content = 'get_filename_component(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../../.." ABSOLUTE)\n\n'
+        content += "\n".join(
+            f"if(NOT TARGET {x.stem})\n"
+            f"  add_executable({x.stem} IMPORTED)\n"
+            f'  set_target_properties({x.stem} PROPERTIES IMPORTED_LOCATION "${{_IMPORT_PREFIX}}/bin/{x.name}")\n'
+            "endif()\n"
+            for x in sorted(bin_dir.glob(exe_pattern))
+        )
+        save(self, cmake_file_path, content)
+
+    def _write_export_objects_cmake(self, cmake_file_path):
+        # MLIR sets additional properties on targets when object files are installed, e.g.
+        #   add_library(obj.MLIRCAPIInterfaces OBJECT IMPORTED)
+        #   set_property(TARGET obj.MLIRCAPIInterfaces APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+        #   set_target_properties(obj.MLIRCAPIInterfaces PROPERTIES
+        #     IMPORTED_COMMON_LANGUAGE_RUNTIME_RELEASE ""
+        #     IMPORTED_OBJECTS_RELEASE "${_IMPORT_PREFIX}/lib/objects-Release/obj.MLIRCAPIInterfaces/Interfaces.cpp.o"
+        #     )
+        #   set_target_properties(MLIRCAPIInterfaces PROPERTIES
+        #     MLIR_AGGREGATE_DEP_LIBS_IMPORTED "MLIRInferTypeOpInterface;LLVMSupport"
+        #     MLIR_AGGREGATE_OBJECT_LIB_IMPORTED "obj.MLIRCAPIInterfaces"
+        #   )
+        build_type_upper = str(self.settings.build_type).upper()
+        components = self._build_info["components"]
+        content = ['get_filename_component(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../../.." ABSOLUTE)\n']
+        for name, object_files in self._obj_libs.items():
+            content.append(f"add_library(obj.{name} OBJECT IMPORTED)")
+            content.append(f"set_property(TARGET obj.{name} APPEND PROPERTY IMPORTED_CONFIGURATIONS {build_type_upper})")
+            formatted_obj_files = ";".join("${_IMPORT_PREFIX}/" + str(o.relative_to(self.package_folder)).replace("\\", "/") for o in object_files)
+            content.append(f'set_property(TARGET obj.{name} APPEND PROPERTY IMPORTED_OBJECTS_{build_type_upper} "{formatted_obj_files}")')
+            deps = ";".join(r for r in components[name]["requires"] if r.startswith("MLIR"))
+            content.append(textwrap.dedent(f"""\
+                set_target_properties({name} PROPERTIES
+                    MLIR_AGGREGATE_DEP_LIBS_IMPORTED "{deps}"
+                    MLIR_AGGREGATE_OBJECT_LIB_IMPORTED "obj.{name}"
+                )"""))
+            content.append("\n")
+        save(self, cmake_file_path, "\n".join(content))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "MLIR")
@@ -194,7 +252,13 @@ class MLIRConan(ConanFile):
             self.output.warning(f"Some libraries were not declared as components: {found_libs - component_libs}")
 
         self.cpp_info.builddirs.append(self._cmake_module_path)
-        self.cpp_info.set_property("cmake_build_modules", [self._cmake_module_path / "MLIRConfigVars.cmake"])
+        modules = [
+            self._cmake_module_path / "MLIRConfigVars.cmake",
+            self._cmake_module_path / "conan_add_executable_targets.cmake",
+        ]
+        if self.options.install_aggregate_objects:
+            modules.append(self._cmake_module_path / "conan_add_object_targets.cmake")
+        self.cpp_info.set_property("cmake_build_modules", modules)
 
 
 def parse_dotfile(dotfile, label_replacements=None):
