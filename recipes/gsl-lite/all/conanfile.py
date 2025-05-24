d22432f71504546f936c1b5ee884da10050efd98
Moritz Beutel <mbeutel@users.noreply.github.com>
2025-05-24 12:28:43 +0300

    gsl-lite: Modernize recipe, add versions 0.43.0 and 1.0.1
    Cherry-picked with fixes from https://github.com/conan-io/conan-center-index/pull/27517

--- recipes/gsl-lite/all/conanfile.py
+++ recipes/gsl-lite/all/conanfile.py
@@ -1,45 +1,23 @@
 import os
 
 from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain
 from conan.tools.files import *
 from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
 
 class GslLiteConan(ConanFile):
     name = "gsl-lite"
-    description = "A single-file header-only version of ISO C++ " \
-                  "Guideline Support Library (GSL) for C++98, C++11 and later"
+    description = "ISO C++ Core Guidelines Library implementation for C++98, C++11 up"
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://github.com/martinmoene/gsl-lite"
+    homepage = "https://gsl-lite.github.io/gsl-lite/"
     topics = ("GSL", "header-only")
     package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
-    #  There are three configuration options for this GSL implementation's behavior
-    #  when pre/post conditions on the GSL types are violated:
-    #
-    #  1. GSL_TERMINATE_ON_CONTRACT_VIOLATION: std::terminate will be called (default)
-    #  2. GSL_THROW_ON_CONTRACT_VIOLATION: a gsl::fail_fast exception will be thrown
-    #  3. GSL_UNENFORCED_ON_CONTRACT_VIOLATION: nothing happens
-    #
-    options = {
-        "on_contract_violation": ["terminate", "throw", "unenforced"]
-    }
-    default_options = {
-        "on_contract_violation": "terminate",
-    }
-
-    no_copy_source = True
-
-    @property
-    def _contract_map(self):
-        return {
-            "terminate": "GSL_TERMINATE_ON_CONTRACT_VIOLATION",
-            "throw": "GSL_THROW_ON_CONTRACT_VIOLATION",
-            "unenforced": "GSL_UNENFORCED_ON_CONTRACT_VIOLATION"
-        }
 
     def layout(self):
         basic_layout(self, src_folder="src")
@@ -50,13 +28,33 @@ class GslLiteConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
     def package(self):
-        copy(self, "*", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
         copy(self, "LICENSE",  src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+        if Version(self.version) >= "1.0.0":
+            rmdir(self, os.path.join(self.package_folder, "share"))
+        else:
+            rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "gsl-lite")
-        self.cpp_info.set_property("cmake_target_name", "gsl::gsl-lite")
-        self.cpp_info.bindirs = []
-        self.cpp_info.libdirs = []
-        self.cpp_info.defines = [self._contract_map[str(self.options.on_contract_violation)]]
+        self.cpp_info.set_property("cmake_target_name", "gsl-lite::gsl-lite")
+        version_major = Version(self.version).major
+        self.cpp_info.set_property("cmake_target_aliases", [f"gsl-lite::gsl-lite-v{version_major}"])
+
+        # The official CMake config also defines both gsl-lite::gsl-lite-v0 and gsl-lite::gsl-lite-v1 targets,
+        # and adds a -Dgsl_CONFIG_DEFAULTS_VERSION=0 define to the v0 one.
+        # Not reproducing this here to not accidentally break recipes
+        # that are (implicitly) adding gsl-lite::gsl-lite to self.cpp_info.requires,
+        # which would unintentionally link against both targets.
+        # The user can still add that define manually, if necessary.
