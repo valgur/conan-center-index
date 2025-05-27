0855f326b2ee204931d4b3e33e4329e923a58f41
Martin Valgur <martin.valgur@gmail.com>
2025-05-28 01:35:42 +0300

    nanomsg: improve CMake v4 support

--- recipes/nanomsg/all/conanfile.py
+++ recipes/nanomsg/all/conanfile.py
@@ -39,6 +39,9 @@ class NanomsgConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        replace_in_file(self, "CMakeLists.txt",
+                        "cmake_minimum_required (VERSION 2.8.12)",
+                        "cmake_minimum_required (VERSION 3.5)")
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -48,9 +51,6 @@ class NanomsgConan(ConanFile):
         tc.variables["NN_ENABLE_DOC"] = False
         tc.variables["NN_TESTS"] = False
         tc.variables["NN_TOOLS"] = self.options.enable_tools
-        tc.cache_variables["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5" # CMake 4 support
-        if Version(self.version) > "1.2.1":
-            raise ConanException("CMAKE_POLICY_VERSION_MINIMUM hardcoded to 3.5, check if new version supports CMake 4")
         tc.generate()
         tc = CMakeDeps(self)
         tc.generate()
