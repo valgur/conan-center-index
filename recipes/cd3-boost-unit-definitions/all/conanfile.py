05ba77a343cb2dd4f3e70feed7206f27e8d9c055
Martin Valgur <martin.valgur@gmail.com>
2025-05-31 19:54:43 +0300

    cd3-boost-unit-definitions: tidy

--- recipes/cd3-boost-unit-definitions/all/conanfile.py
+++ recipes/cd3-boost-unit-definitions/all/conanfile.py
@@ -1,11 +1,9 @@
+import os
+
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import *
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
-import os
-
 
 required_conan_version = ">=2.1"
 
@@ -21,19 +19,6 @@ class Cd3BoostUnitDefinitionsConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    @property
-    def _min_cppstd(self):
-        return 14
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "msvc": "191",
-            "gcc": "5",
-            "clang": "5",
-            "apple-clang": "5.1",
-        }
-
     def layout(self):
         basic_layout(self, src_folder="src")
 
@@ -44,43 +29,18 @@ class Cd3BoostUnitDefinitionsConan(ConanFile):
         self.info.clear()
 
     def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(
-            str(self.settings.compiler), False
-        )
-        if (
-            minimum_version
-            and Version(self.settings.compiler.version) < minimum_version
-        ):
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 14)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def build(self):
-        pass
-
     def package(self):
-        copy(
-            self,
-            pattern="LICENSE.md",
-            dst=os.path.join(self.package_folder, "licenses"),
-            src=self.source_folder,
-        )
-        copy(
-            self,
-            pattern="*.hpp",
-            dst=os.path.join(self.package_folder, "include"),
-            src=os.path.join(self.source_folder, "src"),
-        )
+        copy(self, "LICENSE.md", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(self, "*.hpp", os.path.join(self.source_folder, "src"), os.path.join(self.package_folder, "include"))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "BoostUnitDefinitions")
+        self.cpp_info.set_property("cmake_target_name", "BoostUnitDefinitions::BoostUnitDefinitions")
+
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
-
-        self.cpp_info.set_property("cmake_file_name", "BoostUnitDefinitions")
-        self.cpp_info.set_property(
-            "cmake_target_name", "BoostUnitDefinitions::BoostUnitDefinitions"
-        )
