24066846048b8ff5c2cbfa962bfeb05801081828
Martin Valgur <martin.valgur@gmail.com>
2025-05-31 19:46:48 +0300

    cctag: adjust boost components, drop old versions

--- recipes/cctag/all/conanfile.py
+++ recipes/cctag/all/conanfile.py
@@ -1,12 +1,9 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import *
-from conan.tools.microsoft import is_msvc_static_runtime
-from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
@@ -55,42 +52,22 @@ class CCTagConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     @property
+    def _boost_components(self):
+        return ["atomic", "chrono", "date_time", "exception", "filesystem", "math_c99", "random", "serialization", "system", "thread", "timer"]
+
+    @property
     def _apps_opencv_components(self):
         return ["videoio", "imgproc", "imgcodecs", "highgui"]
 
     def requirements(self):
-        # boost/1.85.0 not compatible because of "error: 'numeric' is not a namespace-name" error
-        boost_version = "1.86.0" if Version(self.version) >= "1.0.4" else "1.84.0"
-        self.requires(f"boost/[^1.71.0]", transitive_headers=True, transitive_libs=True)
+        self.requires("boost/[^1.71.0]", transitive_headers=True, transitive_libs=True,
+                      options={f"with_{comp.replace('_c99', '')}": True for comp in self._boost_components + ["stacktrace"]})
         self.requires("eigen/3.4.0", transitive_headers=True)
-        if Version(self.version) >= "1.0.3":
-            self.requires("onetbb/[^2021]")
-        else:
-            self.requires("onetbb/[^2020.3]")
-        extra_opts = {comp: True for comp in self._apps_opencv_components} if self.options.apps else {}
-        self.requires("opencv/[^4.5]", transitive_headers=True, transitive_libs=True, options=extra_opts)
-
-    @property
-    def _required_boost_components(self):
-        return [
-            "atomic", "chrono", "date_time", "exception", "filesystem",
-            "math", "serialization", "stacktrace", "system", "thread", "timer",
-        ]
+        self.requires("onetbb/[^2021]")
+        self.requires("opencv/[^4.5]", transitive_headers=True, transitive_libs=True,
+                      options={comp: True for comp in self._apps_opencv_components} if self.options.apps else {})
 
     def validate(self):
-        miss_boost_required_comp = any(
-            self.dependencies["boost"].options.get_safe(f"without_{boost_comp}", True)
-            for boost_comp in self._required_boost_components
-        )
-        if self.dependencies["boost"].options.header_only or miss_boost_required_comp:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires non header-only boost with these components: "
-                f"{', '.join(self._required_boost_components)}",
-            )
-
-        if is_msvc_static_runtime(self) and not self.options.shared and self.dependencies["onetbb"].options.shared:
-            raise ConanInvalidConfiguration("this specific configuration is prevented due to internal c3i limitations")
-
         check_min_cppstd(self, 14)
 
     def source(self):
@@ -104,12 +81,6 @@ class CCTagConan(ConanFile):
         replace_in_file(self, os.path.join("src", "CMakeLists.txt"), "${OpenCV_LIBS}", "opencv_core")
         # Cleanup RPATH if Apple in shared lib of install tree
         replace_in_file(self, "CMakeLists.txt", "SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)", "")
-        # From https://github.com/alicevision/CCTag/pull/210/files CCTAG_CUDA_CC_LIST_INIT0 variable doesn't exists anymore in favor of a chooseCudaCC() cmake function
-        if Version(self.version) < "1.0.4":
-            # Remove very old CUDA compute capabilities
-            replace_in_file(self, "CMakeLists.txt",
-                            "set(CCTAG_CUDA_CC_LIST_INIT0 3.5 3.7 5.0 5.2)",
-                            "set(CCTAG_CUDA_CC_LIST_INIT0 5.0 5.2)")
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -152,24 +123,13 @@ class CCTagConan(ConanFile):
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.extend(["dl", "pthread"])
         self.cpp_info.requires = [
-            "boost::atomic",
-            "boost::chrono",
-            "boost::date_time",
-            "boost::exception",
-            "boost::filesystem",
-            "boost::math_c99",
-            "boost::serialization",
-            "boost::system",
-            "boost::thread",
-            "boost::timer",
             "eigen::eigen",
             "onetbb::onetbb",
             "opencv::opencv_core",
         ]
+        self.cpp_info.requires.extend([f"boost::{comp}" for comp in self._boost_components])
         if self.options.apps:
-            self.cpp_info.requires.extend([
-                f"opencv::opencv_{comp}" for comp in self._apps_opencv_components
-            ])
+            self.cpp_info.requires.extend([f"opencv::opencv_{comp}" for comp in self._apps_opencv_components])
         if self.settings.os == "Windows":
             self.cpp_info.requires.append("boost::stacktrace_windbg")
         else:
