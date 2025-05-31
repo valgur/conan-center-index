39e4378da47759e2d7b37d11b980bb192a1bd704
Martin Valgur <martin.valgur@gmail.com>
2025-05-31 22:46:35 +0300

    deco: tidy

--- recipes/deco/all/conanfile.py
+++ recipes/deco/all/conanfile.py
@@ -1,11 +1,9 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import *
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
@@ -20,20 +18,8 @@ class DecoConan(ConanFile):
 
     package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
+
     no_copy_source = True
-
-    @property
-    def _min_cppstd(self):
-        return 17
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "msvc": "191",
-            "gcc": "7",
-            "clang": "5.0",
-            "apple-clang": "9.1",
-        }
 
     def layout(self):
         basic_layout(self, src_folder="src")
@@ -48,21 +34,7 @@ class DecoConan(ConanFile):
         self.info.clear()
 
     def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
-
-        compiler = str(self.settings.compiler)
-        compiler_version = Version(self.settings.compiler.version)
-
-        if compiler not in self._compilers_minimum_version:
-            self.output.info(f"{self.name} requires a compiler that supports at least C++17")
-            return
-
-        # Exclude compilers not supported
-        if compiler_version < self._compilers_minimum_version[compiler]:
-            raise ConanInvalidConfiguration(
-                f"{self.name} requires a compiler that supports at least C++17. "
-                f"{compiler} {Version(self.settings.compiler.version.value)} is not"
-            )
+        check_min_cppstd(self, 17)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
