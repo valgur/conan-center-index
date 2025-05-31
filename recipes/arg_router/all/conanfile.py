15cd2f65e0bdef0e91e9a1a87551b611fbe3452a
Martin Valgur <martin.valgur@gmail.com>
2025-05-31 17:07:29 +0300

    arg_router: clean up, bump min boost version

--- recipes/arg_router/all/conanfile.py
+++ recipes/arg_router/all/conanfile.py
@@ -2,8 +2,10 @@ import os
 
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain, CMakeDeps
 from conan.tools.files import *
+
+required_conan_version = ">=2.1"
 
 
 class ArgRouterRecipe(ConanFile):
@@ -13,45 +15,48 @@ class ArgRouterRecipe(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     description = "C++ command line argument parsing and routing."
     topics = ("cpp", "command-line", "argument-parser", "header-only")
-
-    # CMake >= 3.18 is required https://github.com/cmannett85/arg_router/blob/449567723d6c0e9db0a4c89277066c9a53b299fa/CMakeLists.txt#L5
-    tool_requires = "cmake/[>=3.25.3 <5]"
-    requires = "boost/[^1.71.0]", "span-lite/[>=0.10.3 <1]"
-
     settings = "os", "arch", "compiler", "build_type"
     package_type = "header-library"
-    generators = "CMakeDeps", "CMakeToolchain"
     no_copy_source = True
 
-    def validate(self):
-        check_min_cppstd(self, 17)
-
-    def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+    def package_id(self):
+        self.info.clear()
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
+    def requirements(self):
+        self.requires("boost/[^1.74.0]")
+        self.requires("span-lite/[>=0.10.3 <1]")
+
+    def validate(self):
+        check_min_cppstd(self, 17)
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.25.3 <5]")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["INSTALLATION_ONLY"] = True
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
+
     def build(self):
         cmake = CMake(self)
-        cmake.configure(variables={"INSTALLATION_ONLY": True})
+        cmake.configure()
         cmake.build()
 
     def package(self):
         cmake = CMake(self)
         cmake.install()
-
         rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
-        # Folders not used for header-only
+        self.cpp_info.set_property("cmake_file_name", "arg_router")
+        self.cpp_info.set_property("cmake_target_name", "arg_router::arg_router")
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
-
-        self.cpp_info.set_property("cmake_file_name", "arg_router")
-        self.cpp_info.set_property(
-            "cmake_target_name", "arg_router::arg_router")
-
-    def package_id(self):
-        # build_type and compiler are needed for the Conan's CMake tools but are not actually used
-        self.info.clear()
