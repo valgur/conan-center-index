9a901605b1404cbd21e7642bed960ad01576057b
Martin Valgur <martin.valgur@gmail.com>
2025-06-01 18:03:15 +0300

    redboltz-mqtt_cpp: tidy

--- recipes/redboltz-mqtt_cpp/all/conanfile.py
+++ recipes/redboltz-mqtt_cpp/all/conanfile.py
@@ -1,11 +1,9 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import *
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
@@ -22,19 +20,6 @@ class MqttCPPConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    @property
-    def _min_cppstd(self):
-        return 14
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "gcc": "6",
-            "msvc": "191",
-            "clang": "5",
-            "apple-clang": "10",
-        }
-
     def layout(self):
         basic_layout(self, src_folder="src")
 
@@ -45,13 +30,7 @@ class MqttCPPConan(ConanFile):
         self.info.clear()
 
     def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
-
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 14)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
