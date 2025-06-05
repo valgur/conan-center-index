e2412c8390c0d7389ae3ed935c0e1309c6abaa5d
Martin Valgur <martin.valgur@gmail.com>
2025-06-06 00:28:00 +0300

    gettext: fix test_package

--- recipes/gettext/all/test_package/conanfile.py
+++ recipes/gettext/all/test_package/conanfile.py
@@ -10,7 +10,7 @@ class TestPackageConan(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
-        self.requires(self.tested_reference_str)
+        self.requires(self.tested_reference_str, run=True)
 
     def layout(self):
         cmake_layout(self)
