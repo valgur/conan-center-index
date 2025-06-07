0e7cbbe335be133328eda31605476cf825beab6b
Martin Valgur <martin.valgur@gmail.com>
2025-06-07 22:40:08 +0300

    librdata: clean up

--- recipes/librdata/all/conanfile.py
+++ recipes/librdata/all/conanfile.py
@@ -1,14 +1,16 @@
 import os
 
 from conan import ConanFile
-from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain
-from conan.tools.files import copy, rm, rmdir, get
+from conan.tools import CppInfo
 from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.microsoft import is_msvc
+from conan.tools.env import Environment
+from conan.tools.files import *
+from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain
 from conan.tools.layout import basic_layout
-from conan.errors import ConanInvalidConfiguration
+from conan.tools.microsoft import is_msvc, unix_path
 
-required_conan_version = ">=2.0.9"
+required_conan_version = ">=2.1"
+
 
 class Libreadstat(ConanFile):
     name = "librdata"
@@ -32,22 +34,15 @@ class Libreadstat(ConanFile):
     def layout(self):
         basic_layout(self, src_folder="src")
 
-    def validate(self):
-        # INFO: rdata is not prepared for Visual Studio, but mingw and msys2 only
-        # It fails with configure:4072: error: C compiler cannot create executables
-        # The upstream uses mingw in the CI, not MSVC
-        if is_msvc(self):
-            raise ConanInvalidConfiguration(f"{self.ref} does not support Visual Studio. Please use MinGW or MSYS2.")
-
     def requirements(self):
-        self.requires("bzip2/1.0.8")
-        self.requires("zlib/[>=1.2.11 <2]")
+        self.requires("bzip2/[^1.0.8]")
+        self.requires("zlib-ng/[^2.0]")
         self.requires("xz_utils/[>=5.4.5 <6]")
 
     def build_requirements(self):
-        self.tool_requires("libtool/2.4.7")
-        # INFO: gettext is required by libtool due macro: AM_ICONV
-        self.tool_requires("gettext/0.22.5")
+        self.tool_requires("libtool/[^2.4.7]")
+        # gettext is required by libtool due macro: AM_ICONV
+        self.tool_requires("gettext/[>=0.21 <1]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -55,8 +50,21 @@ class Libreadstat(ConanFile):
     def generate(self):
         tc = AutotoolsToolchain(self)
         tc.generate()
-        dep = AutotoolsDeps(self)
-        dep.generate()
+
+        if is_msvc(self):
+            cpp_info = CppInfo(self)
+            for dependency in self.dependencies.values():
+                cpp_info.merge(dependency.cpp_info.aggregated_components())
+            env = Environment()
+            env.append("CPPFLAGS", [f"-I{unix_path(self, p)}" for p in cpp_info.includedirs] + [f"-D{d}" for d in cpp_info.defines])
+            env.append("_LINK_", [lib if lib.endswith(".lib") else f"{lib}.lib" for lib in cpp_info.libs])
+            env.append("LDFLAGS", [f"-L{unix_path(self, p)}" for p in cpp_info.libdirs] + cpp_info.sharedlinkflags + cpp_info.exelinkflags)
+            env.append("CXXFLAGS", cpp_info.cxxflags)
+            env.append("CFLAGS", cpp_info.cflags)
+            env.vars(self).save_script("conanautotoolsdeps_cl_workaround")
+        else:
+            deps = AutotoolsDeps(self)
+            deps.generate()
 
     def build(self):
         autotools = Autotools(self)
@@ -65,15 +73,14 @@ class Libreadstat(ConanFile):
         autotools.make()
 
     def package(self):
-        copy(self, pattern="LICENSE*", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, "LICENSE*", self.source_folder, os.path.join(self.package_folder, "licenses"))
         autotools = Autotools(self)
         autotools.install()
         rm(self, "*.la", os.path.join(self.package_folder, "lib"))
-        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
-        rmdir(self, os.path.join(self.package_folder, "share"))
         fix_apple_shared_install_name(self)
 
     def package_info(self):
+        # The project does not export a .pc file
         suffix = "_i" if is_msvc(self) and self.options.shared else ""
         self.cpp_info.libs = [f"rdata{suffix}"]
         if self.settings.os in ("FreeBSD", "Linux"):
