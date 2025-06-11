2a5abb4545e9e0634e0e56572824b067c11667f4
Martin Valgur <martin.valgur@gmail.com>
2025-06-11 17:54:57 +0300

    make: add i18n support

--- recipes/make/all/conanfile.py
+++ recipes/make/all/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc, VCVars
 
-required_conan_version = ">=2.1"
+required_conan_version = ">=2.4"
 
 
 class MakeConan(ConanFile):
@@ -21,16 +21,27 @@ class MakeConan(ConanFile):
     license = "GPL-3.0-or-later"
     package_type = "application"
     settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "i18n": [True, False],
+    }
+    default_options = {
+        "i18n": False,
+    }
+    languages = ["C"]
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.i18n
 
     def export_sources(self):
         export_conandata_patches(self)
 
-    def configure(self):
-        self.settings.rm_safe("compiler.cppstd")
-        self.settings.rm_safe("compiler.libcxx")
-
     def layout(self):
         basic_layout(self, src_folder="src")
+
+    def build_requirements(self):
+        if self.options.get_safe("i18n"):
+            self.tool_requires("gettext/[>=0.21 <1]")
 
     def package_id(self):
         del self.info.settings.compiler
@@ -45,6 +56,7 @@ class MakeConan(ConanFile):
             vcvars.generate()
         if self.settings_build.os != "Windows":
             tc = AutotoolsToolchain(self)
+            tc.configure_args.append("--enable-nls" if self.options.i18n else "--disable-nls")
             tc.generate()
 
     def build(self):
@@ -57,10 +69,17 @@ class MakeConan(ConanFile):
         else:
             autotools = Autotools(self)
             autotools.configure()
+            if self.options.i18n:
+                with chdir(self, os.path.join(self.build_folder, "po")):
+                    autotools.make()
             self.run("./build.sh")
 
     def package(self):
         copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        if self.options.get_safe("i18n"):
+            with chdir(self, os.path.join(self.build_folder, "po")):
+                autotools = Autotools(self)
+                autotools.install()
         for make_exe in ("make", "*gnumake.exe"):
             src = self.source_folder if self.settings_build.os == "Windows" else self.build_folder
             copy(self, make_exe, src, os.path.join(self.package_folder, "bin"), keep_path=False)
@@ -68,6 +87,8 @@ class MakeConan(ConanFile):
     def package_info(self):
         self.cpp_info.includedirs = []
         self.cpp_info.libdirs = []
+        if self.options.get_safe("i18n"):
+            self.cpp_info.resdirs = ["share"]
 
         make = os.path.join(self.package_folder, "bin", "gnumake.exe" if self.settings.os == "Windows" else "make")
         self.conf_info.define("tools.gnu:make_program", make)
