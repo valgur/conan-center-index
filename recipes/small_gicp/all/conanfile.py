76b9bd2fdea7bcda60acd8bdabf5961b6fed043d
Martin Valgur <martin.valgur@gmail.com>
2025-06-20 12:55:20 +0300

    small_gicp: use a version range for llvm-opemp

--- recipes/small_gicp/all/conanfile.py
+++ recipes/small_gicp/all/conanfile.py
@@ -40,7 +40,7 @@ class IridescenceConan(ConanFile):
         if self.options.with_openmp:
             # '#pragma omp' is used in public headers
             # Note: native MSVC OpenMP is not compatible
-            self.requires("llvm-openmp/18.1.8", transitive_headers=True, transitive_libs=True)
+            self.requires("llvm-openmp/[*]", transitive_headers=True, transitive_libs=True)
         if self.options.with_tbb:
             self.requires("onetbb/[>=2021 <2023]", transitive_headers=True, transitive_libs=True)
 
