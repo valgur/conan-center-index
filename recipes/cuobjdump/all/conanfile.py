391bf4d5af173d63b8e614b37a5e2717e71ca021
Martin Valgur <martin.valgur@gmail.com>
2025-08-02 22:59:42 +0300

    cuobjdump: add recipe

---
+++ recipes/cuobjdump/all/conanfile.py
@@ -0,0 +1,45 @@
+import os
+
+from conan import ConanFile
+from conan.tools.files import *
+from conan.tools.layout import basic_layout
+
+required_conan_version = ">=2.1"
+
+
+class CuobjdumpConan(ConanFile):
+    name = "cuobjdump"
+    description = "Utility to extract information from CUDA binary files"
+    license = "DocumentRef-LICENSE:LicenseRef-NVIDIA-End-User-License-Agreement"
+    homepage = "https://docs.nvidia.com/cuda/cuda-binary-utilities/"
+    topics = ("cuda", "utilities", "cubin", "ptx")
+    package_type = "application"
+    settings = "os", "arch", "compiler", "build_type"
+
+    python_requires = "conan-utils/latest"
+
+    @property
+    def _utils(self):
+        return self.python_requires["conan-utils"].module
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        del self.info.settings.compiler
+        del self.info.settings.build_type
+
+    def validate(self):
+        self._utils.validate_cuda_package(self, "cuda_cuobjdump")
+
+    def build(self):
+        self._utils.download_cuda_package(self, "cuda_cuobjdump")
+
+    def package(self):
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(self, "*", os.path.join(self.source_folder, "bin"), os.path.join(self.package_folder, "bin"))
+
+    def package_info(self):
+        self.cpp_info.libdirs = []
+        self.cpp_info.includedirs = []
+        self.cpp_info.resdirs = []
