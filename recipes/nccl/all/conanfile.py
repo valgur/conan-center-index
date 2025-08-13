8a9efee32430f40c7663880ca228772c4f388d81
Martin Valgur <martin.valgur@gmail.com>
2025-08-13 14:53:06 +0300

    nccl: use the correct compiler, add system lib deps

--- recipes/nccl/all/conanfile.py
+++ recipes/nccl/all/conanfile.py
@@ -2,7 +2,7 @@ import os
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd
+from conan.tools.build import check_min_cppstd, stdcpp_library
 from conan.tools.files import *
 from conan.tools.gnu import AutotoolsToolchain, Autotools
 from conan.tools.layout import basic_layout
@@ -79,17 +79,20 @@ class NcclConan(ConanFile):
         ldflags = " ".join(f"-L{dep.cpp_info.libdirs[0]}" for _, dep in self.dependencies.host.items() if dep.cpp_info.libdirs)
 
         tc = AutotoolsToolchain(self)
+        tc_vars = tc.vars()
         tc.make_args.append(f"DEBUG={'1' if self.settings.build_type == 'Debug' else '0'}")
         tc.make_args.append("NVCC=nvcc")
         tc.make_args.append(f"CUDA_HOME={self.dependencies['cudart'].package_folder}")
         tc.make_args.append(f"CUDA_LIB={self.dependencies['cudart'].cpp_info.libdirs[0]}")
         tc.make_args.append(f"NVCC_GENCODE={' '.join(nvcc_tc.cudaflags)}")
         tc.make_args.append(f"INCFLAGS+={incflags}")
-        tc.make_args.append(f"CXXFLAGS+={incflags} -fPIC")
-        tc.make_args.append(f"LDFLAGS+={ldflags}")
+        tc.make_args.append(f"CXXFLAGS+={incflags} -fPIC {tc_vars.get('CXXFLAGS', '')} {tc_vars.get('CPPFLAGS', '')}")
+        tc.make_args.append(f"LDFLAGS+={ldflags} {tc_vars.get('LDFLAGS', '')}")
         tc.make_args.append(f"NVLDFLAGS+={ldflags} {' '.join(nvcc_tc.cudaflags)}")
         tc.make_args.append(f"RDMA_CORE={'1' if self.options.with_ibverbs else '0'}")
         tc.make_args.append(f"MLX5DV={'1' if self.options.with_mlx5 else '0'}")
+        tc.make_args.append(f"CC={tc_vars.get('CC', 'cc')}")
+        tc.make_args.append(f"CXX={tc_vars.get('CXX', 'c++')}")
         if self.conf.get("tools.build:verbosity", "quiet") == "verbose":
             tc.make_args.append("VERBOSE=1")
         tc.generate()
@@ -113,6 +116,10 @@ class NcclConan(ConanFile):
     def package_info(self):
         self.cpp_info.set_property("pkg_config_name", "nccl")
         self.cpp_info.libs = ["nccl" if self.options.shared else "nccl_static"]
+        if not self.options.shared:
+            self.cpp_info.system_libs = ["m", "pthread", "dl", "rt"]
+            if stdcpp_library(self):
+                self.cpp_info.system_libs.append(stdcpp_library(self))
         self.cpp_info.requires = ["cudart::cudart_"]
         if self.options.with_ibverbs:
             self.cpp_info.requires.append("rdma-core::libibverbs")
