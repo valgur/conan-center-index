7408a4aa3ba54ab958f03c80ae3d7a7fa0ed064e
Martin Valgur <martin.valgur@gmail.com>
2025-08-13 23:33:56 +0300

    nvjitlink: fix test_package for older versions

--- recipes/nvjitlink/all/test_package/test_package.c
+++ recipes/nvjitlink/all/test_package/test_package.c
@@ -1,12 +1,6 @@
 #include <nvJitLink.h>
-#include <stdio.h>
 
 int main() {
-    unsigned int major, minor;
-    nvJitLinkResult status = nvJitLinkVersion(&major, &minor);
-    if (status != NVJITLINK_SUCCESS) {
-        printf("nvJitLink API error: %d\n", status);
-        return 1;
-    }
-    printf("nvJitLink version: %d.%d\n", major, minor);
+    nvJitLinkHandle handle;
+    nvJitLinkComplete(handle);
 }
