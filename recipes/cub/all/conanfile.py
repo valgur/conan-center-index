df960528ceaf7d1f189d0ddb7680f01f0640a29c
Martin Valgur <martin.valgur@gmail.com>
2025-08-14 15:39:13 +0300

    cub: use the official CMake config

--- recipes/cub/all/conanfile.py
+++ recipes/cub/all/conanfile.py
@@ -32,20 +32,55 @@ class CubConan(ConanFile):
     def validate(self):
         check_min_cppstd(self, 14)
 
+    @property
+    def _cmake_dir(self):
+        if Version(self.version) >= "2.8":
+            return os.path.join(self.source_folder, "lib/cmake/cub")
+        elif Version(self.version) >= "2.2":
+            return os.path.join(self.source_folder, "cub/cub/cmake")
+        else:
+            return os.path.join(self.source_folder, "cub/cmake")
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        if Version(self.version) >= "2.2":
-            move_folder_contents(self, os.path.join(self.source_folder, "cub"), self.source_folder)
+        cmake_config = os.path.join(self._cmake_dir, "cub-config.cmake")
+        replace_in_file(self, cmake_config,
+                        "${_CUB_VERSION_INCLUDE_DIR}",
+                        "${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR}")
+        replace_in_file(self, cmake_config,
+                        "set(CUB_CONFIG",
+                        "# set(CUB_CONFIG")
+        replace_in_file(self, cmake_config,
+                        "find_package_handle_standard_args(",
+                        "# find_package_handle_standard_args(")
 
     def package(self):
-        copy(self, "LICENSE.TXT", self.source_folder, os.path.join(self.package_folder, "licenses"))
-        copy(self, "*", os.path.join(self.source_folder, "cub"), os.path.join(self.package_folder, "include", "cub"))
+        if Version(self.version) >= "2.2":
+            copy(self, "LICENSE.TXT", os.path.join(self.source_folder, "cub"), os.path.join(self.package_folder, "licenses"))
+            copy(self, "*", os.path.join(self.source_folder, "cub", "cub"), os.path.join(self.package_folder, "include", "cub"))
+        else:
+            copy(self, "LICENSE.TXT", self.source_folder, os.path.join(self.package_folder, "licenses"))
+            copy(self, "*", os.path.join(self.source_folder, "cub"), os.path.join(self.package_folder, "include", "cub"))
+        copy(self, "cub-config.cmake", self._cmake_dir, os.path.join(self.package_folder, "lib/cmake"))
+        rename(self, os.path.join(self.package_folder, "lib/cmake/cub-config.cmake"),
+               os.path.join(self.package_folder, "lib/cmake/cub-config-official.cmake"))
 
     def package_info(self):
         # Follows the naming conventions of the official CMake config file:
         # https://github.com/NVIDIA/cccl/blob/main/lib/cmake/cub/cub-config.cmake
         self.cpp_info.set_property("cmake_file_name", "cub")
-        self.cpp_info.set_property("cmake_target_name", "CUB::CUB")
+        # Disable the default target.
+        # CUB::CUB is created in the loaded .cmake module instead.
+        self.cpp_info.set_property("cmake_target_name", "_cub_do_not_use")
+        # The CMake module ensures that the include dir is exported as a non-SYSTEM include in CMake
+        # https://github.com/NVIDIA/cccl/blob/v2.2.0/cub/lib/cmake/cub/cub-config.cmake#L11-L29
+        self.cpp_info.set_property("cmake_build_modules", ["lib/cmake/cub-config-official.cmake"])
+
+        self.cpp_info.bindirs = []
+        self.cpp_info.frameworkdirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
+        self.cpp_info.builddirs = ["lib/cmake"]
 
         self.cpp_info.bindirs = []
         self.cpp_info.frameworkdirs = []
