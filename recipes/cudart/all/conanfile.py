f1669e843ad7cb5a91563236fe1c0580efe42de7
Martin Valgur <martin.valgur@gmail.com>
2025-08-14 18:48:16 +0300

    cudart: need transitive_libs=True for FindCUDAToolkit to work

--- recipes/cudart/all/conanfile.py
+++ recipes/cudart/all/conanfile.py
@@ -44,14 +44,14 @@ class CudartConan(ConanFile):
 
     def requirements(self):
         v = Version(self.version)
-        self.requires(f"cuda-crt/[~{v.major}.{v.minor}]", transitive_headers=True)
+        self.requires(f"cuda-crt/[~{v.major}.{v.minor}]", transitive_headers=True, transitive_libs=True)
         self.requires(f"cuda-driver-stubs/[~{v.major}.{v.minor}]", transitive_headers=True, transitive_libs=True)
         if Version(self.version) >= "13.0":
-            self.requires("cuda-cccl/[^3]", transitive_headers=True)
+            self.requires("cuda-cccl/[^3]", transitive_headers=True, transitive_libs=True)
         elif Version(self.version) >= "12.2":
-            self.requires("cuda-cccl/[^2]", transitive_headers=True)
+            self.requires("cuda-cccl/[^2]", transitive_headers=True, transitive_libs=True)
         else:
-            self.requires("cuda-cccl/[^1]", transitive_headers=True)
+            self.requires("cuda-cccl/[^1]", transitive_headers=True, transitive_libs=True)
 
     def validate(self):
         self._utils.validate_cuda_package(self, "cuda_cudart")
