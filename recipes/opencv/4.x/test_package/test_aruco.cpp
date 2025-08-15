4d55f44d801f4375a8ad35b9d93d82a06d9ac35b
Martin Valgur <martin.valgur@gmail.com>
2025-08-15 08:19:15 +0300

    opencv: fix test_aruco.cpp

--- recipes/opencv/4.x/test_package/test_aruco.cpp
+++ recipes/opencv/4.x/test_package/test_aruco.cpp
@@ -1,9 +1,7 @@
-#include <opencv2/core.hpp>
 #include <opencv2/aruco.hpp>
 
 int main() {
-    cv::Mat markerImage;
     auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-    cv::aruco::drawMarker(dictionary, 23, 200, markerImage, 1);
-    return 0;
+    cv::aruco::CharucoBoard board(cv::Size(5, 7), 0.04f, 0.02f, dictionary);
+    cv::aruco::testCharucoCornersCollinear(&board, cv::Mat::ones(4, 6, CV_32SC1));
 }
