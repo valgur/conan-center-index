05f9cd8abc9c17f2fab09c4ea6f28a80a901a871
Martin Valgur <martin.valgur@gmail.com>
2025-08-18 16:11:33 +0300

    cutlass: add install_examples_headers option for onnxruntime

--- recipes/cutlass/all/conanfile.py
+++ recipes/cutlass/all/conanfile.py
@@ -1,10 +1,10 @@
 import os
-import textwrap
 
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import *
+from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
@@ -13,52 +13,50 @@ class CutlassConan(ConanFile):
     name = "cutlass"
     description = "CUTLASS: CUDA Templates for Linear Algebra Subroutines"
     license = "BSD-3-Clause"
-    url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/NVIDIA/cutlass"
     topics = ("linear-algebra", "gpu", "cuda", "deep-learning", "nvidia", "header-only")
-
     package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
-    # TODO: add header_only=False option
+    options = {
+        "install_examples_headers": [True, False],
+    }
+    default_options = {
+        "install_examples_headers": False,
+    }
+    no_copy_source = True
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def package_id(self):
-        self.info.clear()
+        self.info.settings.clear()
+        self.info.conf.clear()
 
     def validate(self):
-        check_min_cppstd(self, 17)
+        check_min_cppstd(self, 17 if Version(self.version) >= "3.0" else 11)
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.19 <5]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        # Don't look for CUDA, we're only installing the headers
-        replace_in_file(self, "CMakeLists.txt",
-                        "include(${CMAKE_CURRENT_SOURCE_DIR}/CUDA.cmake)",
-                        textwrap.dedent("""
-                            if(NOT CUTLASS_ENABLE_HEADERS_ONLY)
-                                include(${CMAKE_CURRENT_SOURCE_DIR}/CUDA.cmake)
-                            endif()
-                        """))
+        # Don't look for CUDA when only installing headers
+        save(self, os.path.join(self.source_folder, "CUDA.cmake"), "")
 
     def generate(self):
         # Install via CMake to ensure headers are configured correctly
         tc = CMakeToolchain(self)
-        tc.cache_variables["CMAKE_SUPPRESS_REGENERATION"] = True
-        tc.cache_variables["CUTLASS_REVISION"]=f"v{self.version}"
-        tc.cache_variables["CUTLASS_NATIVE_CUDA"] = False
-        tc.cache_variables["CUTLASS_ENABLE_HEADERS_ONLY"] = True
+        tc.cache_variables["CUTLASS_ENABLE_HEADERS_ONLY"] = True  # non-header-only is only used for tests, tools and examples
+        tc.cache_variables["CUTLASS_ENABLE_LIBRARY"] = True
         tc.cache_variables["CUTLASS_ENABLE_TOOLS"] = False
-        tc.cache_variables["CUTLASS_ENABLE_LIBRARY"] = False
-        tc.cache_variables["CUTLASS_ENABLE_PROFILER"] = False
-        tc.cache_variables["CUTLASS_ENABLE_PERFORMANCE"] = False
+        tc.cache_variables["CUTLASS_NATIVE_CUDA"] = False
+        tc.cache_variables["CUTLASS_ENABLE_CUBLAS"] = False  # used only in tests
+        tc.cache_variables["CUTLASS_ENABLE_CUDNN"] = False  # used only in tests
+        tc.cache_variables["CUTLASS_ENABLE_PROFILER"] = False  # tool
+        tc.cache_variables["CUTLASS_ENABLE_PERFORMANCE"] = False  # tool
         tc.cache_variables["CUTLASS_ENABLE_TESTS"] = False
-        tc.cache_variables["CUTLASS_ENABLE_GTEST_UNIT_TESTS"] = False
-        tc.cache_variables["CUTLASS_ENABLE_CUBLAS"] = False
-        tc.cache_variables["CUTLASS_ENABLE_CUDNN"] = False
+        tc.cache_variables["CUTLASS_INSTALL_TESTS"] = False
+        tc.cache_variables["CMAKE_SUPPRESS_REGENERATION"] = True
         tc.generate()
 
     def build(self):
@@ -70,6 +68,11 @@ class CutlassConan(ConanFile):
         copy(self, "LICENSE.txt", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
+        if self.options.install_examples_headers:
+            for pattern in ["*.h", "*.hpp", "*.cuh"]:
+                copy(self, pattern,
+                     os.path.join(self.source_folder, "examples"),
+                     os.path.join(self.package_folder, "include", "cutlass", "examples"))
         rmdir(self, os.path.join(self.package_folder, "lib"))
         rmdir(self, os.path.join(self.package_folder, "test"))
 
@@ -78,3 +81,5 @@ class CutlassConan(ConanFile):
         self.cpp_info.set_property("cmake_target_name", "nvidia::cutlass::cutlass")
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
+        if self.options.install_examples_headers:
+            self.cpp_info.includedirs.append(os.path.join("include", "cutlass", "examples"))
