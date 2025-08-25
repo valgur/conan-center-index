3494877a46b4bf7f029f76e905f166aae884489b
Martin Valgur <martin.valgur@gmail.com>
2025-08-25 14:29:01 +0300

    jasper: simplify custom CMake module

--- recipes/jasper/all/conanfile.py
+++ recipes/jasper/all/conanfile.py
@@ -53,7 +53,7 @@ class JasperConan(ConanFile):
 
     def build_requirements(self):
         if Version(self.version) >= "4.1.1":
-            self.tool_requires("cmake/[>=3.20 <5]")
+            self.tool_requires("cmake/[>=3.20]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -98,21 +98,7 @@ class JasperConan(ConanFile):
         if self.settings.os == "Windows":
             for dll_prefix in ["concrt", "msvcp", "vcruntime"]:
                 rm(self, f"{dll_prefix}*.dll", os.path.join(self.package_folder, "bin"))
-        self._create_cmake_module_variables(os.path.join(self.package_folder, self._module_file_rel_path))
-
-    # FIXME: Missing CMake alias variables. See https://github.com/conan-io/conan/issues/7691
-    def _create_cmake_module_variables(self, module_file):
-        content = textwrap.dedent(f"""\
-            set(JASPER_FOUND TRUE)
-            if(DEFINED Jasper_INCLUDE_DIR)
-                set(JASPER_INCLUDE_DIR ${{Jasper_INCLUDE_DIR}})
-            endif()
-            if(DEFINED Jasper_LIBRARIES)
-                set(JASPER_LIBRARIES ${{Jasper_LIBRARIES}})
-            endif()
-            set(JASPER_VERSION_STRING "{self.version}")
-        """)
-        save(self, module_file, content)
+        save(self, os.path.join(self.package_folder, self._module_file_rel_path), "set(JASPER_FOUND TRUE)")
 
     @property
     def _module_file_rel_path(self):
@@ -122,6 +108,7 @@ class JasperConan(ConanFile):
         self.cpp_info.set_property("cmake_find_mode", "both")
         self.cpp_info.set_property("cmake_file_name", "Jasper")
         self.cpp_info.set_property("cmake_target_name", "Jasper::Jasper")
+        self.cpp_info.set_property("cmake_additional_variables_prefixes", ["JASPER"])
         self.cpp_info.set_property("cmake_build_modules", [self._module_file_rel_path])
         self.cpp_info.set_property("pkg_config_name", "jasper")
         self.cpp_info.libs = ["jasper"]
