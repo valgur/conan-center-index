18daad7aa4e2f92210dbf8d252dece319702ba2d
Martin Valgur <martin.valgur@gmail.com>
2025-08-25 11:36:30 +0300

    jitify: bump to v2

--- recipes/jitify/all/conanfile.py
+++ recipes/jitify/all/conanfile.py
@@ -1,8 +1,10 @@
 import os
 
 from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.files import *
 from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
@@ -15,6 +17,14 @@ class JitifyConan(ConanFile):
     topics = ("cuda", "jit", "nvrtc", "header-only")
     package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type", "cuda"
+    options = {
+        "v2": [True, False],
+        "with_nvtx": [True, False],
+    }
+    default_options = {
+        "v2": True,
+        "with_nvtx": True,
+    }
 
     python_requires = "conan-utils/latest"
 
@@ -22,17 +32,27 @@ class JitifyConan(ConanFile):
     def _utils(self):
         return self.python_requires["conan-utils"].module
 
+    def configure(self):
+        if not self.options.v2:
+            del self.options.with_nvtx
+
     def layout(self):
         basic_layout(self, src_folder="src")
 
     def package_id(self):
-        self.info.clear()
+        self.info.settings.clear()
 
     def requirements(self):
         self._utils.cuda_requires(self, "nvrtc")
+        if self.options.v2:
+            if Version(self.settings.cuda.version) >= "12.0":
+                self._utils.cuda_requires(self, "nvjitlink")
+            if self.options.with_nvtx:
+                self._utils.cuda_requires(self, "nvtx")
 
     def validate(self):
         self._utils.validate_cuda_settings(self)
+        check_min_cppstd(self, 17)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -40,7 +60,18 @@ class JitifyConan(ConanFile):
     def package(self):
         copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
         copy(self, "jitify.hpp", self.source_folder, os.path.join(self.package_folder, "include"))
+        if self.options.v2:
+            for f in [
+                "jitify2.hpp",
+                "jitify2_preprocess.cpp",
+                "stringify.cpp",
+            ]:
+                copy(self, f, self.source_folder, os.path.join(self.package_folder, "include"))
 
     def package_info(self):
         self.cpp_info.libdirs = []
         self.cpp_info.bindirs = []
+        if self.options.v2:
+            self.cpp_info.srcdirs = ["include"]
+            if self.options.with_nvtx:
+                self.cpp_info.defines.append("JITIFY_ENABLE_NVTX")
