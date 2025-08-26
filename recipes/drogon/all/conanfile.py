4883a64a39d46e7520a0806796f91eaa80df5dad
Martin Valgur <martin.valgur@gmail.com>
2025-08-26 19:14:59 +0300

    use a version range for hiredis

--- recipes/drogon/all/conanfile.py
+++ recipes/drogon/all/conanfile.py
@@ -97,7 +97,7 @@ class DrogonConan(ConanFile):
         if self.options.get_safe("with_sqlite"):
             self.requires("sqlite3/[>=3.45.0 <4]")
         if self.options.get_safe("with_redis"):
-            self.requires("hiredis/1.2.0")
+            self.requires("hiredis/[^1.0]")
         if self.options.get_safe("with_yaml_cpp"):
             self.requires("yaml-cpp/[>=0.8.0 <1]")
 
